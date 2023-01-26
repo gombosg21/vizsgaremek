1ad93cdac6c705f0f15853f95cb52e4bc279f7ad
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-01-26 20:46:44 +0100

    added user model attribute constrictions

--- backend/models/user.js
+++ backend/models/user.js
@@ -14,19 +14,19 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   user.init({
-    ID: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    deleted: DataTypes.BOOLEAN,
-    banned: DataTypes.BOOLEAN,
-    type: DataTypes.INTEGER,
-    register_date: DataTypes.DATE,
-    password: DataTypes.STRING,
-    email_verified: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    last_online: DataTypes.DATE,
-    birth_date: DataTypes.DATEONLY,
-    profile_description: DataTypes.TEXT,
-    profile_visibility: DataTypes.INTEGER
+    ID: {type: DataTypes.INTEGER, allowNull:false, autoIncrement:true,primaryKey:true},
+    name: {type:DataTypes.STRING, allowNull:false, unique:true},
+    deleted: {type:DataTypes.BOOLEAN,allowNull:false,defaultValue: false},
+    banned:{type:DataTypes.BOOLEAN,allowNull:false,defaultValue: false},
+    type:{type:DataTypes.INTEGER,allowNull:false,defaultValue:0},
+    register_date:{type:DataTypes.DATE,allowNull:false,defaultValue:DataTypes.CURRENT_TIMESTAMP},
+    password:{type:DataTypes.STRING,allowNull:false},
+    email_verified:{type:DataTypes.BOOLEAN,allowNull:false,defaultValue: false},
+    email:{type:DataTypes.STRING,allowNull:false},
+    last_online:{type:DataTypes.DATE,allowNull:false},
+    birth_date:{type:DataTypes.DATEONLY,allowNull:false}, 
+    profile_description:{type:DataTypes.TEXT},
+    profile_visibility:{type:DataTypes.INTEGER,allowNull:false,defaultValue:0} 
   }, {
     sequelize,
     modelName: 'user',
