295eb9d6b37f7c8506e091cd356f15f2037bfb8a
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-06 13:03:29 +0100

    set up additional routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,32 +4,34 @@ const router = express.Router();
 
 module.exports = router;
 
-// get user, will return all if logged in as :ID else only a view of it
-
+// get user, for testing now
 router.get('/user/:ID',userController.getUser);
 
-// get login
+//get profile
+router.get('/user/:ID/profile')
 
+// get login
 router.get('/user/:name/login',userController.login);
 
-//get logout
-
+// get logout
 router.get('/user/:ID/logout',)
 
 // get reset password
-
 router.get('/user/:name/password-reset',userController.resetPassword);
 
 // post create user
-
 router.post('/user/register',userController.createUser);
 
 // patch edit profile
+router.patch('/user/:ID/profile/edit');
 
 // patch change password
+router.patch('/user/:ID/change-password',);
 
-// put change user register data
+// patch change user register data
+router.patch('/user/:ID/register/edit')
 
 // delete delete user
+router.delete('/user/:ID',userController.deleteUser);
 
-router.delete('/user/:id',userController.deleteUser);
+// patch add reaction
