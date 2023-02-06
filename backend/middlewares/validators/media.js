fbc4e5ee74f86457c6c2139340033396a1062910
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-06 13:02:46 +0100

    defined validators

--- backend/middlewares/validators/media.js
+++ backend/middlewares/validators/media.js
@@ -1,5 +1,66 @@
 const vailadtor = require('express-vailadtor');
+const tags = require('../../models').tag;
 
-exports.validateUpload = () => {}
+exports.validateUpload = (req,res,next) => 
+{
+    const imgData = req.body.imgData;
+    const placeholder = req.body.placeholder;
+    const userID = req.session.userID;
 
-exports.valiadteEdit = () => {}
+    if (imgData === null || placeholder == null || userID == null) 
+    {
+        res.status(406);
+        return;
+    }
+    else 
+    {
+        next();
+    }
+};
+
+exports.valiadteEdit = (req,res,next) => 
+{
+    const placeholder = req.body.placeholder;
+
+    if (placeholder == null) 
+    {  
+        res.status(406)
+        return;
+    }
+    else 
+    {
+        next()
+    }
+};
+
+exports.validateTags = async (req,res,next) => 
+{
+    const Tags = req.body.tags;
+
+    if( Tags === null) 
+    {
+        res.status(406);
+        return;
+    } 
+    else 
+    {
+        var badTag = false;
+        for(let i = 0; i < Tags.lenght(); i++)
+        {
+            var tag = await tags.findOne(Tags[i]);
+            if(tag === null) 
+            {
+                badTag = true;
+            }    
+        }
+        if (badTag == true) 
+        {
+            res.status(406);
+            return;
+        } 
+        else 
+        {
+            next();
+        }
+    }
+};
