51158058c3d659b533a21fbba6a71020e7aea94a
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-06 13:03:18 +0100

    set up additional routes

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -3,22 +3,24 @@ const mediaController = require('../controllers/media');
 const router = express.Router();
 
 // create media with user_ID
-router.post('/media/:ID',mediaController.uploadMedia())
+router.post('/media/:ID',mediaController.uploadMedia)
 
 // edit media with media_ID
-router.patch('/media/:ID',mediaController.editMedia())
-
-// view media with media_ID
-router.get('/media/:ID',mediaController.viewMedia())
+router.patch('/media/:ID',mediaController.editMedia)
 
 // delete media with media_ID
+router.delete('/media/:ID',mediaController.deleteMedia)
 
-router.delete('/media/:ID',deleteMedia())
+// view media with media_ID
+router.get('/media/:ownerID/:mediaID',mediaController.viewOneMediaFromUser)
 
 // view ALL media with user_ID
+router.get('/media/:ID/all',mediaController.getAllMediaFromUser)
+
+// view media by tags
+router.get('/media/by-tags',)
 
 // add reaction
 
-// add tags
-
-// view reactions
+// add tag
+router.patch('/media/:ID/tags',)
