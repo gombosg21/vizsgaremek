befc5a7eb2d4fbf839f6bd7b63b258930cead19b
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-06 16:11:02 +0100

    refractored to use validator

--- backend/middlewares/validators/user.js
+++ backend/middlewares/validators/user.js
@@ -1,48 +1,22 @@
-const vailadtor = require('express-validator');
+const valiadtor = require('express-validator');
 
+const password_min = 8;
+const password_max = 32;
 
-exports.validateEmail = (email) => {
-    if(vailadtor.check(email).isEmail()) 
-    {
-        next();
-    } 
-    else
-    {
-        res.status(406)
-        .json({"error":"invalid email"})
-        return;
-    }
-   ;
+exports.accountRules = () => 
+{
+    return [
+    valiadtor.body('email').notEmpty().withMessage("email cannot be empty"),
+    valiadtor.body('email').isEmail().withMessage("Invalid email format"),
+    valiadtor.body('password').notEmpty().withMessage("password cannot be empty"),
+    valiadtor.body('password').isLength({min: password_min,max:password_max}).withMessage(`password must be within ${password_min} and ${password_max} characters long`),
+    valiadtor.body('password').isStrongPassword().withMessage("too weak password, try a different one"),
+    valiadtor.body('birth_date').notEmpty().withMessage("select a birth date"),
+    valiadtor.body('birth_date').isDate().withMessage("invalid date format"),
+    valiadtor.body('gender').notEmpty().withMessage("select a gender"),
+    validator.body('gender').isInt({min:0,max:2}).withMessage("unknow gender type")
+]
 }
-
-exports.validatePassword = (req,res,next) => {
-
-    const password = req.body.password;
-    const minLenght = 8;
-    const maxLenght = 32;
-
-    const regex = new RegExp("/^[0-9]+[a-z]+[A-Z]+$/")
-    
-    if(password.lenght() < minLenght && password.lenght() > maxLenght) 
-    {
-       if (password.test(regex)) 
-       {
-            next();
-       } 
-       else 
-       {
-        res.status(406)
-        .json({"error":"password must contain least one capital letter, one number and one special character"})
-        return;
-       }
-    } 
-    else
-    {
-        res.status(406)
-        .json({"error":`password must be whitin ${minLenght} and ${maxLenght} charaters long.`})
-        return;
-    }
-};
 
 exports.checkIfNameExsits = async (req,res,next) => 
 {
@@ -57,4 +31,19 @@ exports.checkIfNameExsits = async (req,res,next) =>
     {
         next();
     }
+}
+
+exports.validateRegistration = async (req,res,next) => 
+{
+    const errors = valiadtor.validationResult(req);
+
+    if (errors.isEmpty()) 
+    {
+       return next();
+    }
+
+    const errorList = [];
+    errors.array().map(err => errorList.push({[err.param]:err.msg}))
+
+    return res.status(422).json({error: errorList})
 }
