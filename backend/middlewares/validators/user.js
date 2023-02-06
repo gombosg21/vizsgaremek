6b10373650a9451026861b307b3f1d1d52fa2fad
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-06 13:44:29 +0100

    defined initial logic

--- backend/middlewares/validators/user.js
+++ backend/middlewares/validators/user.js
@@ -2,25 +2,59 @@ const vailadtor = require('express-validator');
 
 
 exports.validateEmail = (email) => {
-   return vailadtor.check(email).isEmail();
+    if(vailadtor.check(email).isEmail()) 
+    {
+        next();
+    } 
+    else
+    {
+        res.status(406)
+        .json({"error":"invalid email"})
+        return;
+    }
+   ;
 }
 
-exports.validatePassword = (password) => {
+exports.validatePassword = (req,res,next) => {
+
+    const password = req.body.password;
+    const minLenght = 8;
+    const maxLenght = 32;
 
     const regex = new RegExp("/^[0-9]+[a-z]+[A-Z]+$/")
-    if(password.lenght() < 8 && password.lenght() > 32) 
+    
+    if(password.lenght() < minLenght && password.lenght() > maxLenght) 
     {
        if (password.test(regex)) 
        {
-            return "ok"
+            next();
        } 
        else 
        {
-            return "char"
+        res.status(406)
+        .json({"error":"password must contain least one capital letter, one number and one special character"})
+        return;
        }
     } 
     else
     {
-        return "lenght"
+        res.status(406)
+        .json({"error":`password must be whitin ${minLenght} and ${maxLenght} charaters long.`})
+        return;
     }
 };
+
+exports.checkIfNameExsits = async (req,res,next) => 
+{
+    const UserName = req.body.name;
+
+    if(await user.findOne({where:{name: UserName}}) != null) 
+    {
+        res.status(406)
+        .json({"error":"username already exists"})
+        return;
+    } else 
+    {
+        next();
+    }
+}
