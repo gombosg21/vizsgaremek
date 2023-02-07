8017bba43783f2660656988acaae4fb41c09a112
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-07 21:22:25 +0100

    init, needs further work

---
+++ backend/controllers/comment.js
@@ -0,0 +1,19 @@
+const comment = require('../models').comment;
+const thread = require('../models').thread;
+
+exports.createComment = async (req,res,next) => 
+{
+    const userID = "";
+    const threadID = "";
+    const commentData = req.body.comment_text;
+}
+
+exports.editComment = async (req,res,next) => 
+{
+
+}
+
+exports.deleteComment = async (req,res,next) => 
+{
+
+}
