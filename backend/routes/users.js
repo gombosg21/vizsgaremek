2a1bd7b72d31253b52e7e4b0509b39c70a55a477
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-07 21:21:17 +0100

    refactored routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,36 +2,33 @@ const router = require('express').Router();
 const userController = require('../controllers/user');
 const userValidator = require('../middlewares/validators/user');
 
-// get user, for testing now
-router.get('/user/:userID',userValidator.checkIfUserIDExsits,userController.getUser);
-
-//get profile
-router.get('/user/:userID/profile',userValidator.checkIfUserIDExsits,userController.getProfile)
+// get profile
+// edit profile
+// delete user
+router.route('/user/:userID')
+    .get( userValidator.checkIfUserIDExsits, userController.getProfile)
+    .patch(userValidator.checkIfUserIDExsits, userController.editProfile)
+    .delete( userValidator.checkIfUserIDExsits, userController.deleteUser);
 
 // get login
-router.get('/user/:name/login',userValidator.checkIfNameExsist,userController.login);
+router.get('/user/:name/login', userValidator.checkIfNameExsist, userController.login);
 
 // get logout
-router.get('/user/:userID/logout',userValidator.checkIfUserIDExsits,userController.logout)
+router.get('/user/:userID/logout', userValidator.checkIfUserIDExsits, userController.logout)
 
 // get reset password
-router.get('/user/:name/password-reset',userValidator.checkIfNameExsist,userController.resetPassword);
+router.get('/user/:name/password-reset', userValidator.checkIfNameExsist, userController.resetPassword);
 
 // post create user
-router.post('/user/register',userValidator.checkIfNameConflicts,userValidator.registerRules,userValidator.validateRegistration,userController.createUser);
-
-// patch edit profile
-router.patch('/user/:userID/profile/edit',userValidator.checkIfUserIDExsits,userController.editProfile);
+router.post('/user/register', userValidator.checkIfNameConflicts, userValidator.registerRules, userValidator.validateRegistration, userController.createUser);
 
 // patch change password
-router.patch('/user/:userID/change-password',userValidator.checkIfUserIDExsits,userController.changePassword);
+router.patch('/user/:userID/change-password', userValidator.checkIfUserIDExsits, userController.changePassword);
 
 // patch change user register data
 router.patch('/user/:userID/register/edit')
 
-// delete delete user
-router.delete('/user/:userID',userValidator.checkIfUserIDExsits,userController.deleteUser);
-
-// patch add reaction
+// get search user by params name, birthdate, gender
+router.get('/user/search/:name.:date.:gender')
 
 module.exports = router;
