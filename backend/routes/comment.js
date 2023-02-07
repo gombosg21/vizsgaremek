36bc7a436d327e0232f16cb405ab96562494f2c6
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-07 21:46:13 +0100

    initial routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const commentController = require('../controllers/comment');
+
+// on thread
+// create comment
+// edit comment
+// delete comment
+router.route('/thread/:thread_ID/comment/:comment_ID')
+    .post(commentController.createComment)
+    .patch(commentController.editComment)
+    .delete(commentController.deleteComment);
+
+module.exports = router;
