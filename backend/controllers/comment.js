1d90ee2444f346412f4ea00040866a3aea2edf1e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-07 21:46:24 +0100

    initial logic

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,19 +1,59 @@
 const comment = require('../models').comment;
 const thread = require('../models').thread;
 
-exports.createComment = async (req,res,next) => 
-{
+exports.createComment = async (req, res, next) => {
     const userID = "";
-    const threadID = "";
+    const threadID = req.params.thread_ID;
     const commentData = req.body.comment_text;
+
+    try {
+        const Comment = comment.build({
+            user_Id: userID,
+            thread_ID: threadID,
+            content: commentData
+        });
+        Comment.save();
+        res.status(200);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(502);
+    }
 }
 
-exports.editComment = async (req,res,next) => 
-{
+exports.editComment = async (req, res, next) => {
+    const threadId = req.params.thread_ID;
+    const commentID = req.params.comment_ID;
+    const commentData = req.body.comment_text;
 
+    try {
+        const Comment = await comment.findOne({ where: { ID: commentID } });
+
+        Comment.set({
+            content: commentData
+        });
+        Comment.save();
+        res.status(200).
+            json(Comment);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(502);
+    }
 }
 
-exports.deleteComment = async (req,res,next) => 
-{
+exports.deleteComment = async (req, res, next) => {
 
+    const commentID = req.params.comment_ID;
+
+    try {
+        const Comment = await comment.findOne({ where: { ID: commentID } });
+        await Comment.destroy();
+        res.status(200)
+            .json(Comment.ID);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(502);
+    }
 }
