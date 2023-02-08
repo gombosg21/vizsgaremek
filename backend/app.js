590d1d583c58054805776abd7e8fb77a5562e5f9
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-08 12:38:22 +0100

    shot at cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const DB = require('./util/db')
 
@@ -12,6 +13,12 @@ const mediaRouter = require('./routes/media');
 
 const app = express();
 
+const corsOptions = {
+
+  origin : ['http://localhost/'],
+  optionsSuccessStatus: 200,
+};
+
 DB.init()
 
 app.use(logger('dev'));
@@ -19,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 // app.use('/', indexRouter);
 app.use(usersRouter);
