d21fa189c6079924b4bde933a8f185ca6f0446e9
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-08 21:43:57 +0100

    refactor

--- backend/util/db.js
+++ backend/util/db.js
@@ -1,27 +1,37 @@
- const { Sequelize } = require('sequelize');
- const mysql = require('mysql2/promise')
- const config = require('../config/config.json')
+const mysql = require('mysql2/promise');
+const config = require('../config/config.json');
 
- const mode = "development"
+const mode = "development"
 
-exports.init = async () => {
-    const {database,username,password,host,port,dialect} = config[mode]
-    // console.log(`database:${database},username:${username},password:${password},host:${host},port:${port},dialect:${dialect}`)
-    try 
-    {
-    const connection = await mysql.createConnection({
-        host: host,
-        user : username,
-        password: password,
-        port: port
-    });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_hungarian_ci' ;`)
+const { database, username, password, host, port, dialect } = config[mode]
+
+const connection = mysql.createConnection({
+    host: host,
+    user: username,
+    password: password,
+    port: port
+});
+
+exports.connect = async () => {
+    try {
+        (await connection).connect();
+        console.log(`connection to ${host}:${port} was successfully established`)
     }
-    catch (error) 
-    {
+    catch (error) {
         console.log(error);
     }
-    
+}
+
+exports.initDB = async () => {
+    try {
+        (await connection).query(`CREATE DATABASE IF NOT EXISTS \`${database}\` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_hungarian_ci' ;`)
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+exports.connection = connection;
 
     // const sequelize = new Sequelize(database,username,password,{dialect: dialect})
-};
+
