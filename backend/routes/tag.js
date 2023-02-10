3d09836f28b0c401381e397ad5d2805ec7e61bbb
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-10 16:50:30 +0100

    fixed route logic

--- backend/routes/tag.js
+++ backend/routes/tag.js
@@ -9,14 +9,14 @@ const commonValidator = require("../middlewares/validators/common");
 //delete tag
 router.route("/tag/:ID")
     .patch(tagValidator.checkIfTagIDDoesNotExsits,tagValidator.tagRules(),commonValidator.validate,tagController.updateTag)
-    .delete(tagValidator.checkIfTagIDDoesNotExsits,tagValidator,tagController.deleteTag);
+    .delete(tagValidator.checkIfTagIDDoesNotExsits,tagController.deleteTag);
 
     
 // search tag by name    
 router.get("/tag",tagValidator.searchRules(),commonValidator.validate,tagController.findTags);
 
 //create new tag
-router.post("/tag",tagValidator.tagRules(),commonValidator.validate,checkIfTagNameAlreadyExsits,tagController.deleteTag);
+router.post("/tag",tagValidator.tagRules(),commonValidator.validate,tagValidator.checkIfTagNameAlreadyExsits,tagController.deleteTag);
 
 
 module.exports = router;
