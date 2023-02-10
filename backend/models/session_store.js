6ff09c1ff93d525f67c0b34bb25a952a2ab54ec5
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-02-10 16:50:15 +0100

    fixed model logic

--- backend/models/session_store.js
+++ backend/models/session_store.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.user,{foreignKey:"user_ID"})
+      this.belongsTo(models.user,{foreignKey:"user_ID",targetKey:"ID"})
     }
   }
   session_store.init({
