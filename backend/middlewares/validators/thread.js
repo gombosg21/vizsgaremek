f9e4d74a99daa4b7315c203e56fb8a65942e6d2a
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-02 12:32:22 +0100

    comma paranoia

--- backend/middlewares/validators/thread.js
+++ backend/middlewares/validators/thread.js
@@ -2,11 +2,18 @@ const validator = require('express-validator');
 const comment = require('../models').comment;
 const thread = require('../models').thread;
 
-exports.threadRules = () => 
-{
+exports.createThreadRules = () => {
     return[
         validator.body("thread_name").notEmpty().withMessage("thread must have a name"),
-        validator.body("thread_name").isAscii().withMessage("only letters and numbers and simple special characters are allowed"),
+        validator.body("thread_name").isAscii().withMessage("only letters and numbers and simple characters are allowed"),
+        validator.body("thread_name").isLength({max:100}).withMessage("thread name must be max 100 characters long")
+    ]
+};
+
+exports.editThreadRules = () => {
+    return[
+        validator.body("thread_name").notEmpty().withMessage("thread must have a name"),
+        validator.body("thread_name").isAscii().withMessage("only letters and numbers and simple characters are allowed"),
         validator.body("thread_name").isLength({max:100}).withMessage("thread name must be max 100 characters long")
     ]
 };
@@ -15,12 +22,10 @@ exports.checkIfThreadExsits = async (req,res,next) => {
 
     const threadID = req.params.thread_ID;
 
-    if(await thread.findOne({where:{ID : threadID}}) === null) 
-    {
-        return res.status(406).json({"error":`thread with id:${threadID} does not exsits`});
-    } 
-    else
-    {
+    if(await thread.findOne({where:{ID : threadID}}) === null) {
+        return res.status(406)
+            .json({"error":`thread with id:${threadID} does not exsits`});
+    } else {
        return next(); 
-    }
-}
+    };
+};
