f2d358f2f0bf36cb84dcdce1c81c5377906061ff
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-09 11:30:44 +0100

    corrected imports

--- backend/seeders/99999999999991-media.js
+++ backend/seeders/99999999999991-media.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 const http = require('http');
 const fake = require('@faker-js/faker');
-const user = require('../models/user');
-const media = require('../models/media');
-const tag = require('../models/tag');
+const user = require('../models').user;
+const media = require('../models').media;
+const tag = require('../models').tag;
+const mediaData = require('../helpers/seeding/image-data');
 
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    var mediaArray = [];
-    var userIDs = await user.findAll({ attributes: ['ID'] });
-    var tags = await tag.findAll({ attributes: ['ID'] });
-    var mediaFiles = [];
+    const mediaArray = [];
+    const userIDs = await user.findAll({ attributes: ['ID'] });
+    const tags = await tag.findAll({ attributes: ['ID'] });
+    const mediaFiles = await mediaData.getTemp(10);
 
+    if (mediaFiles) {
 
+      userIDs.forEach(async userID => {
+        var userUploadCount = Math.floor(Math.random() * 10);
 
+        for (let i = 0; i < userUploadCount; i++) {
 
-    userIDs.forEach(async userID => {
-      var userUploadCount = Math.floor(Math.random() * 10);
+          var uploadTagCount = Math.floor(Math.random() * tags.length);
+          const uploadTagIDList = [];
 
-      for (let i = 0; i < userUploadCount; i++) {
-
-        var uploadTagCount = Math.floor(Math.random() * tags.length);
-        var uploadTagIDList = [];
-
-        while (uploadTagIDList.length < uploadTagCount) {
-          var randomTagID = tags[Math.round(Math.random() * tags.length)];
-          if (!uploadTagIDList.includes(randomTagID)) {
-            uploadTagIDList.push({ID:randomTagID});
+          while (uploadTagIDList.length < uploadTagCount) {
+            var randomTagID = tags[Math.round(Math.random() * tags.length)];
+            if (!uploadTagIDList.includes(randomTagID)) {
+              uploadTagIDList.push({ ID: randomTagID });
+            };
           };
-        };
 
           var upload = await media.create({
             user_ID: userID,
@@ -46,10 +46,13 @@ module.exports = {
             }]
           });
           mediaArray.push(upload);
-      };
-    });
+        };
+      });
 
-    await queryInterface.bulkInsert('media',mediaArray);
+      await queryInterface.bulkInsert('media', mediaArray);
+    } else {
+      throw new Error("mediadata missing");
+    };
   },
 
   async down(queryInterface, Sequelize) {
