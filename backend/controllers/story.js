7c0a2ae24190e16bf3d707bc3989270ad4cb0e75
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-12 14:15:29 +0100

    init

---
+++ backend/controllers/story.js
@@ -0,0 +1,44 @@
+const media_story = require('../models').media_story;
+
+exports.createStory = async (req, res, next) => {
+    const userID = req.user.ID;
+    try { 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    };
+};
+
+exports.getStory = async (req, res, next) => {
+    try { 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    };
+};
+
+exports.editStory = async (req, res, next) => {
+    const userID = req.user.ID;
+    try { 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    };
+};
+
+exports.deleteStory = async (req, res, next) => {
+    const userID = req.user.ID;
+    try { 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    };
+};
+
+
+
+
