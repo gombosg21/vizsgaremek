e28675458f122c99b16920c5f4d12920832398cb
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-14 21:52:13 +0100

    moved preprocessquerytags, as it is a preporcessor

--- backend/middlewares/validators/media.js
+++ backend/middlewares/validators/media.js
@@ -33,23 +33,6 @@ exports.checkIfMediaIDExsist = async (req, res, next) => {
     };
 };
 
-exports.preProrcessQueryTags = async (req, res, next) => {
-    const tagNames = req.query.tags;
-    const tagIDs = [];
-    const validTagNames = [];
-
-    for (var tagName of tagNames) {
-        var singleTag = await tag.findOne({ where: { name: tagName }, attributes: ['ID'] });
-        if (singleTag != null) {
-            tagIDs.push(singleTag.ID);
-            validTagNames.push(singleTag.name);
-        };
-    };
-    req.query.tagIDs = tagIDs;
-    req.query.tagNames = validTagNames;
-    next();
-};
-
 exports.validateUploadFile = (req, res, next) => {
     const mediaData = req.file;
 
