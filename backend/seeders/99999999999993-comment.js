d59080ef807dbd77625e4231744cdd9c6113b34a
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-14 13:56:29 +0100

    added logic, crashes w bad assoc, fixme smwh

--- backend/seeders/99999999999993-comment.js
+++ backend/seeders/99999999999993-comment.js
@@ -1,25 +1,60 @@
+const thread = require('../models').thread;
+const user = require('../models').user;
+const fake = require('@faker-js/faker');
+
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+
+    const comments = [];
+
+    const threadIDsraw = await thread.findAll({ include: ['ID'] });
+    const threadIDs = [];
+
+    threadIDsraw.forEach(thread => {
+      threadIDs.push(thread.ID)
+    });
+
+    const userIDsraw = await user.findAll({ attributes: ['ID'] });
+    const userIDs = [];
+    userIDsraw.forEach(user => {
+      userIDs.push(user.ID)
+    });
+
+    const threadCommentCounts = [];
+
+    threadIDs.forEach(threadID => {
+      var ravCommentCount = Math.floor(Math.random() * 10)
+      var commentCount = ravCommentCount > 0 ? ravCommentCount : 1;
+      threadCommentCounts.push({ [threadID]: commentCount });
+    });
+
+    for (let i = 0; i < threadCommentCounts.length; i++) {
+
+      const keys = Object.keys(threadCommentCounts[i]);
+      const threadID = Number(keys[0]);
+      const commentCount = threadCommentCounts[i][threadID];
+
+      var a = 0;
+      while (a < commentCount) {
+        const commenterID = userIDs[Math.floor(Math.random() * userIDs.length)];
+        const newComment = {
+          thread_ID: threadID,
+          user_ID: commenterID,
+          content: fake.faker.lorem.paragraph()
+        };
+        a++;
+        comments.push(newComment);
+      };
+    };
+
+    await queryInterface.bulkInsert();
+
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete();
   }
 };
