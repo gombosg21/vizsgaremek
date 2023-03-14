567c34f61d031c4bacd10c06d3045e103e660887
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-14 13:09:03 +0100

    seeding logic

--- backend/seeders/99999999999992-thread.js
+++ backend/seeders/99999999999992-thread.js
@@ -1,25 +1,58 @@
+const fake = require('@faker-js/faker');
+const thread = require('../models').thread;
+const user = require('../models').user;
+const media = require('../models').media;
+
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const Threads = [];
+
+    const userIDsraw = await user.findAll({ attributes: ['ID'] });
+    const userIDs = [];
+    userIDsraw.forEach(user => {
+      userIDs.push(user.ID)
+    });
+
+    const mediaIDsraw = await media.findAll({ attributes: ['ID'] });
+    const mediaIDs = [];
+    mediaIDsraw.forEach(media => {
+      mediaIDs.push(media.ID)
+    });
+
+    const mediaThreadCounts = [];
+
+    mediaIDs.forEach(mediaID => {
+      var threadCount = Math.floor(Math.random() * 10);
+      mediaThreadCounts.push({ [mediaID]: threadCount });
+    });
+
+
+    for (let i = 0; i < mediaThreadCounts.length; i++) {
+
+      const keys = Object.keys(mediaThreadCounts[i]);
+      const mediaID = Number(keys[0]);
+      const threadCount = mediaThreadCounts[i][mediaID];
+
+      if (threadCount > 0) {
+        const uploaderID = userIDs[Math.floor(Math.random() * userIDs.length)];
+        const newThread = {
+          media_ID: mediaID,
+          user_ID: uploaderID,
+          name: fake.faker.lorem.sentence()
+        };
+        console.log(newThread)
+        Threads.push(newThread);
+      };
+    };
+    await queryInterface.bulkInsert('threads',Threads);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('threads');
   }
 };
