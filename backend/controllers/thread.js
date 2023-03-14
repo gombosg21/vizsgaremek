1f8510dbdc257796f3bcb2fa3fe76f4d7bc08a80
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-14 21:51:24 +0100

    getAll, getByID works, create partially

--- backend/controllers/thread.js
+++ backend/controllers/thread.js
@@ -1,12 +1,15 @@
 const thread = require("../models").thread;
 const comment = require("../models").comment;
+const user = require("../models").user;
+const media = require("../models").media;
+// const story = require("../models").story;
 
 exports.getThread = async (req, res, next) => {
 
     const threadID = req.params.ID;
 
     try {
-        const data = await thread.findOne({ where: { ID: threadID }, include: { model: comment } })
+        const data = await thread.findOne({ where: { ID: threadID }, include: [{ model: comment,attributes:['content','ID','created','last_edit'],include:[{model:user,attributes:['ID','name']}] }, { model: user, attributes: ['ID', 'name'] }] });
         res.status(200)
             .json(data);
     }
@@ -14,7 +17,18 @@ exports.getThread = async (req, res, next) => {
     (error) {
         console.error(error);
         res.status(500);
-    }
+    };
+};
+
+exports.getAllThreads = async (req, res, next) => {
+
+    try {
+        const threadList = await thread.findAll({ attributes: ['ID', 'name', 'status', 'created', 'last_activity'], include: [{ model: user, attributes: ['ID', 'name'] }] });
+
+        res.status(200).json(threadList);
+    } catch (error) {
+        console.error(error)
+    };
 };
 
 
@@ -22,44 +36,66 @@ exports.CreateThread = async (req, res, next) => {
 
     const parentID = req.params.ID;
     const parentType = req.params.parent_type;
-    const threadName = req.body.thread_name;
-    const threadDate = req.body.date;
-    const userID = req.body.user_id;
+    const threadName = req.body.name;
+    const userID = req.user.ID;
+
+    console.log(parentID)
+    console.log(parentType)
+    console.log(threadName)
+    console.log(userID)
 
     try {
-        const Thread = await thread.build({
-            name: threadName,
-            date: threadDate,
-            user: userID,
-        });
-        await Thread.save();
-        res.status(200)
-            .json(Thread);
+        switch (parentType) {
+            case ("user"): {
+                const targetUserID = await user.findOne({ where: { ID: parentID } }).ID;
+
+                break
+            }
+            case ("media"): {
+                const targetMediaID = await media.findOne({ where: { ID: parentID } }).ID;
+                const Thread = await thread.create({
+                    name: threadName,
+                    user_ID: userID,
+                    media_ID: parentID
+                });
+                res.status(200)
+                    .json({ "ID": Thread.ID });
+                break
+            }
+            // case ("story"): {
+            //     const targetStoryID = await story.findOne({where:{ID:parentID}}).ID;
+            //     break
+            // }
+            default: {
+                res.status(500).json({ error: "server-side error" })
+                break
+            };
+        };
     } catch
     (error) {
         console.error(error);
         res.status(500);
-    }
+    };
 };
 
 exports.editThread = async (req, res, next) => {
 
     const threadID = req.params.ID;
-    const threadName = req.body.thread_name;
-    const threadLocked = req.body.thread_locked;
+    const threadName = req.body.name;
+    const threadStatus = req.body.status;
 
     try {
         const Thread = await thread.findOne(threadID);
         Thread.set({
             name: threadName,
-            locked: threadLocked
+            status: threadStatus
         });
         res.status(200)
             .json(Thread);
     } catch (error) {
         console.error(error);
         res.status(500);
-    }
+    };
 };
 
 exports.deleteTread = async (req, res, next) => {
