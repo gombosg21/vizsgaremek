6d8de9da4070d5af19bd74ba14ce948ecb577fa7
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-15 17:05:20 +0100

    added auth, validation, restructured routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const commentController = require('../controllers/comment');
+const commentValidator = require('../middlewares/validators/comment');
+const threadValidator = require('../middlewares/validators/thread');
+const commonValidator = require("../middlewares/validators/common");
+const auth = require("../middlewares/authentiaction/auth");
+const ownership = require("../middlewares/authentiaction/ownership");
 
 // on thread
 // create comment
+router.post('/thread/:threadID/comment', auth.isAuth, threadValidator.checkIfThreadExsits, commentValidator.commentRules(), commonValidator.validate, commentController.createComment);
+
 // edit comment
 // delete comment
-router.route('/thread/:thread_ID/comment/:comment_ID')
-    .post(commentController.createComment)
-    .patch(commentController.editComment)
-    .delete(commentController.deleteComment);
-
+// get
+router.route('/comment/:commentID')
+    .patch(commentValidator.checkIfCommentIDExsist, auth.isAuth, ownership.isMyComment, commentValidator.commentRules(), commonValidator.validate, commentController.editComment)
+    .delete(commentValidator.checkIfCommentIDExsist, auth.isAuth, ownership.isMyComment, commentController.deleteComment)
+    .get(commentValidator.checkIfCommentIDExsist, commentController.getComment);
 module.exports = router;
