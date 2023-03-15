68838f409440614148a4a24cdb633ea5d8ab541c
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-15 17:03:46 +0100

    fixed logic errors + more consistency in return

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,10 +1,12 @@
 const comment = require('../models').comment;
+const user = require('../models').user;
 const thread = require('../models').thread;
 
 exports.getComment = async (req, res, next) => {
-    const commentID = req.params.comment_ID;
+    const commentID = req.params.commentID;
     try {
-        const Comment = comment.findOne({where:{ID: commentID}});
+        const Comment = await comment.findByPk(commentID,
+            { attributes: ['ID', 'content'], include: [{ model: user, attributes: ['ID', 'name'] }, { model: thread, attributes: ['ID', 'name'] }] });
 
         res.status(200)
             .json(Comment);
@@ -16,17 +18,17 @@ exports.getComment = async (req, res, next) => {
 
 exports.createComment = async (req, res, next) => {
     const userID = req.user.ID;
-    const threadID = req.params.thread_ID;
-    const commentData = req.body.comment_text;
+    const threadID = req.params.threadID;
+    const commentData = req.body.content;
 
     try {
-        const Comment = comment.build({
-            user_Id: userID,
+        const Comment = await comment.build({
+            user_ID: userID,
             thread_ID: threadID,
             content: commentData
         });
-        Comment.save();
-        res.status(200);
+        await Comment.save();
+        res.status(200).json({ ID: Comment.ID });
     }
     catch (error) {
         console.error(error);
@@ -35,18 +37,23 @@ exports.createComment = async (req, res, next) => {
 };
 
 exports.editComment = async (req, res, next) => {
-    const commentID = req.params.comment_ID;
-    const commentData = req.body.comment_text;
+    const commentID = req.params.commentID;
+    const commentData = req.body.content;
 
     try {
-        const Comment = await comment.findOne({ where: { ID: commentID } });
+        const Comment = await comment.findByPk(commentID);
 
         Comment.set({
-            content: commentData ?? Comment.content
+            content: commentData,
+            last_edit: Date.now()
         });
         Comment.save();
         res.status(200).
-            json(Comment);
+            json({
+                ID: Comment.ID,
+                content: Comment.content,
+                last_edit: Comment.last_edit
+            });
     }
     catch (error) {
         console.error(error);
@@ -56,13 +63,13 @@ exports.editComment = async (req, res, next) => {
 
 exports.deleteComment = async (req, res, next) => {
 
-    const commentID = req.params.comment_ID;
+    const commentID = req.params.commentID;
 
     try {
-        const Comment = await comment.findOne({ where: { ID: commentID } });
+        const Comment = await comment.findByPk(commentID);
         await Comment.destroy();
         res.status(200)
-            .json(Comment.ID);
+            .json({ ID: Comment.ID });
     }
     catch (error) {
         console.error(error);
