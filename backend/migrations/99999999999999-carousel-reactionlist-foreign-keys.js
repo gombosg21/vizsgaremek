90feeb4ddbd826e26f0d27c6df6d3345469d4dfe
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-17 21:29:06 +0100

    fixed constraint name

--- backend/migrations/99999999999999-carousel-reactionlist-foreign-keys.js
+++ backend/migrations/99999999999999-carousel-reactionlist-foreign-keys.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        queryInterface.addConstraint('carousel_reactionlists', { fields: ['carousel_ID'], type: 'foreign key', name: 'fk_carousel_reactionlist_media', references: { table: 'carousel', field: 'ID' } });
+        queryInterface.addConstraint('carousel_reactionlists', { fields: ['carousel_ID'], type: 'foreign key', name: 'fk_carousel_reactionlist_carousel', references: { table: 'carousels', field: 'ID' } });
         queryInterface.addConstraint('carousel_reactionlists', { fields: ['user_ID'], type: 'foreign key', name: 'fk_carousel_reactionlist_user', references: { table: 'users', field: 'ID' } });
         queryInterface.addConstraint('carousel_reactionlists', { fields: ['reaction_ID'], type: 'foreign key', name: 'fk_carousel_reactionlist_reaction', references: { table: 'reactions', field: 'ID' } });
     },
