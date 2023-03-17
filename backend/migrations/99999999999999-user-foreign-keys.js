11d633a3f2cd6e51c0a038a2cb54249244cfcad0
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-17 21:16:14 +0100

    removed constraint

--- backend/migrations/99999999999999-user-foreign-keys.js
+++ backend/migrations/99999999999999-user-foreign-keys.js
@@ -3,10 +3,9 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
         queryInterface.addConstraint('users', { fields: ['profile_pic'], type: 'foreign key', name: 'fk_prof_pic', references: { table: 'media', field: 'ID' } });
-        queryInterface.addConstraint('users', { fields: ['profile_thread'], type: 'foreign key', name: 'fk_prof_thread', references: { table: 'threads', field: 'ID' } });
     },
     async down(queryInterface, Sequelize) {
         queryInterface.removeConstraint('users', 'fk_prof_pic');
-        queryInterface.removeConstraint('users', 'fk_prof_thread');
+
     }
 };
