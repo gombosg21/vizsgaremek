11f6008c5f0766a33b5b4a5ddd06c9c5a7ae4784
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-17 22:45:26 +0100

    more specific errors

--- backend/util/password.js
+++ backend/util/password.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 exports.generatePassword = async (password) => {
 
     if (!password) { throw new Error("attibute password missing"); };
-    if (typeof password != "string") { throw new Error("password must be a string"); };
+    if (typeof password != "string") { throw new TypeError("password must be a string"); };
 
     const salt = crypto.randomBytes(256).toString('hex');
 
@@ -24,17 +24,17 @@ exports.validatePassword = (password, salt, hash) => {
     const regexCharMap = /[0-9a-fA-F]+/;
 
     if (!password) { throw new Error("attibute password missing"); };
-    if (typeof password != "string") { throw new Error("password must be a string"); };
+    if (typeof password != "string") { throw new TypeError("password must be a string"); };
 
     if (!salt) { throw new Error("attibute salt missing"); };
-    if (typeof salt != "string") { throw new Error("salt must be a string"); };
-    if (salt.length != 512) { throw new Error("salt must be 512 characters long") };
-    if (salt.match(regexCharMap)[0] != salt) { throw new Error("salt must be a hexadecimal string"); };
+    if (typeof salt != "string") { throw new TypeError("salt must be a string"); };
+    if (salt.length != 512) { throw new RangeError("salt must be 512 characters long") };
+    if (salt.match(regexCharMap)[0] != salt) { throw new TypeError("salt must be a hexadecimal string"); };
 
     if (!hash) { throw new Error("attibute hash missing"); };
-    if (typeof hash != "string") { throw new Error("hash must be a string"); };
-    if (hash.length != 512) { throw new Error("hash must be 512 characters long") };
-    if (hash.match(regexCharMap)[0] != hash) { throw new Error("hash must be a hexadecimal string"); };
+    if (typeof hash != "string") { throw new TypeError("hash must be a string"); };
+    if (hash.length != 512) { throw new RangeError("hash must be 512 characters long") };
+    if (hash.match(regexCharMap)[0] != hash) { throw new TypeError("hash must be a hexadecimal string"); };
 
     return new Promise((resolve, rejects) => {
         crypto.scrypt(password, salt, 256, (error, derivedKey) => {
--- backend/util/serialize-file.js
+++ backend/util/serialize-file.js
@@ -1,15 +1,8 @@
 exports.getBase64 = async (fileBuffer) => {
 
-    if (!fileBuffer) {
-        throw new Error("argument fileBuffer missing")
-    };
-    if (!(fileBuffer instanceof Buffer)) {
-        throw new Error("fileBuffer must be a buffer")
-    };
-
-    if (fileBuffer.byteLength == 0) { 
-        throw new Error("fileBuffer cannot be an empty buffer") 
-    };
+    if (!fileBuffer) { throw new Error("argument fileBuffer missing") };
+    if (!(fileBuffer instanceof Buffer)) { throw new TypeError("fileBuffer must be a buffer") };
+    if (fileBuffer.byteLength == 0) { throw new RangeError("fileBuffer cannot be an empty buffer") };
 
     const base64Data = fileBuffer.toString('base64');
 
@@ -17,15 +10,9 @@ exports.getBase64 = async (fileBuffer) => {
 };
 
 exports.getBlob = async (base64Data) => {
-    if (!base64Data) {
-        throw new Error("argument base64Data missing")
-    };
-    if (typeof base64Data != "string") {
-        throw new Error("base64Data must be a string")
-    };
-    if (base64Data.length == 0) {
-        throw new Error("base64Data cannot be an empty string")
-    };
+    if (!base64Data) { throw new Error("argument base64Data missing") };
+    if (typeof base64Data != "string") { throw new TypeError("base64Data must be a string") };
+    if (base64Data.length == 0) { throw new RangeError("base64Data cannot be an empty string") };
 
     const bufferData = await new Buffer.from(base64Data, "base64");
     const blobData = new Blob(bufferData);
