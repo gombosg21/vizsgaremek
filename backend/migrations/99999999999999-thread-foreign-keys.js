71a0df83ba8673eb6ca2e13197965a5a3c950eda
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-17 21:31:58 +0100

    fixed foreign key source

--- backend/migrations/99999999999999-thread-foreign-keys.js
+++ backend/migrations/99999999999999-thread-foreign-keys.js
@@ -5,7 +5,7 @@ module.exports = {
         queryInterface.addConstraint('threads', { fields: ['user_ID'], type: 'foreign key', name: 'fk_thread_user', references: { table: 'users', field: 'ID' } });
         queryInterface.addConstraint('threads', { fields: ['media_ID'], type: 'foreign key', name: 'fk_thread_media', references: { table: 'media', field: 'ID' } });
         queryInterface.addConstraint('threads', { fields: ['profile_ID'], type: 'foreign key', name: 'fk_thread_profile', references: { table: 'users', field: 'ID' } });
-        queryInterface.addConstraint('threads', { fields: ['media_ID'], type: 'foreign key', name: 'fk_thread_carousel', references: { table: 'carousels', field: 'ID' } });
+        queryInterface.addConstraint('threads', { fields: ['carousel_ID'], type: 'foreign key', name: 'fk_thread_carousel', references: { table: 'carousels', field: 'ID' } });
     },
     async down(queryInterface, Sequelize) {
         queryInterface.removeConstraint('threads', 'fk_thread_user');
