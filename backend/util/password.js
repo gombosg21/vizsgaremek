2442237070559ca76e389ddbaf442ac8e012708c
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-20 18:47:12 +0100

    fixed spelling

--- backend/util/password.js
+++ backend/util/password.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 exports.generatePassword = async (password) => {
 
-    if (!password) { throw new Error("attibute password missing"); };
+    if (!password) { throw new Error("attribute password missing"); };
     if (typeof password != "string") { throw new TypeError("password must be a string"); };
 
     const salt = crypto.randomBytes(256).toString('hex');
@@ -23,15 +23,15 @@ exports.generatePassword = async (password) => {
 exports.validatePassword = (password, salt, hash) => {
     const regexCharMap = /[0-9a-fA-F]+/;
 
-    if (!password) { throw new Error("attibute password missing"); };
+    if (!password) { throw new Error("attribute password missing"); };
     if (typeof password != "string") { throw new TypeError("password must be a string"); };
 
-    if (!salt) { throw new Error("attibute salt missing"); };
+    if (!salt) { throw new Error("attribute salt missing"); };
     if (typeof salt != "string") { throw new TypeError("salt must be a string"); };
     if (salt.length != 512) { throw new RangeError("salt must be 512 characters long") };
     if (salt.match(regexCharMap)[0] != salt) { throw new TypeError("salt must be a hexadecimal string"); };
 
-    if (!hash) { throw new Error("attibute hash missing"); };
+    if (!hash) { throw new Error("attribute hash missing"); };
     if (typeof hash != "string") { throw new TypeError("hash must be a string"); };
     if (hash.length != 512) { throw new RangeError("hash must be 512 characters long") };
     if (hash.match(regexCharMap)[0] != hash) { throw new TypeError("hash must be a hexadecimal string"); };
