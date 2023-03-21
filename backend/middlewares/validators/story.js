8b16ef57a93e13a47081ffa8398cdd31b7294c7e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-21 13:26:27 +0100

    more rules

--- backend/middlewares/validators/story.js
+++ backend/middlewares/validators/story.js
@@ -1,12 +1,12 @@
 const validator = require('express-validator');
 const carousel = require('../../models').carousel;
 
-exports.checkIfStoryIDExsits = async (req,res,next) => {
+exports.checkIfStoryIDExsits = async (req, res, next) => {
     const ID = req.params.storyID;
     try {
         const story = await carousel.findByPK(ID);
         if (!story) {
-            return res.status(404).json({"error":`carousel with id : ${ID} does not exsits`});
+            return res.status(404).json({ "error": `carousel with id : ${ID} does not exsits` });
         } else {
             return next();
         }
@@ -18,8 +18,10 @@ exports.checkIfStoryIDExsits = async (req,res,next) => {
 
 exports.storyRules = () => {
     return [
+        validator.body('name').notEmpty().withMessage("name cannot be empty"),
         validator.body('name').isAscii().withMessage("name must be letters numbers and special characters only"),
+        validator.body('description').notEmpty().withMessage('description cannot be empty'),
         validator.body('description').isAscii().withMessage('description must be letters numbers and special characters only'),
-        validator.body('visibility').isNumeric({min:0,max:3}).withMessage('visibility must be a whole positive number in range of 0-3')
+        validator.body('visibility').optional({ nullable: true, checkFalsy: true }).isNumeric({ min: 0, max: 3 }).withMessage('visibility must be a whole positive number in range of 0-3')
     ];
 };
