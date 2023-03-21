17d8643af812a6b44f7b912b5a70158cac15f1e5
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-21 22:20:25 +0100

    fixed logic

--- backend/seeders/99999999999994-carousel.js
+++ backend/seeders/99999999999994-carousel.js
@@ -1,4 +1,5 @@
 const carousel = require('../models').carousel;
+const carousel_medialist = require('../models').carousel_medialist;
 const media = require('../models').media;
 const user = require('../models').user;
 const fake = require('@faker-js/faker');
@@ -8,8 +9,6 @@ const fake = require('@faker-js/faker');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-
     const usersRaw = await user.findAll({ attributes: ['ID'] });
 
     const userIDs = [];
@@ -17,27 +16,37 @@ module.exports = {
       userIDs.push(user.ID);
     });
 
-    const mediaRaw = await media.findAll({ attributes: ['ID'] });
-    const mediaIDs = [];
+    const mediaAssocIDs = [];
+    const mediaRaw = await media.findAll({ include: { model: user } });
     mediaRaw.forEach(media => {
-      mediaIDs.push(media.ID);
+      mediaAssocIDs.push({ [media.user.ID]: media.ID });
     });
 
 
 
     for (ID of userIDs) {
-      const uploadIDs = [];
+      var uploadIDs = [];
+      for (let i = 0; i < mediaAssocIDs.length; i++) {
+        var userID = Number(Object.keys(mediaAssocIDs[i]));
+        var mediaID = mediaAssocIDs[i][userID];
+        if (userID == ID) {
+          uploadIDs.push(mediaID);
+        };
+      };
 
       const newCarousel = await carousel.create({
         user_ID: ID,
         name: fake.faker.lorem.word(),
         description: fake.faker.lorem.sentences(),
-        visibility: Math.floor(Math.random() * 3)
+        visibility: Math.floor(Math.random() * 3),
       });
+
+      await newCarousel.setMedia(uploadIDs);
     };
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('carousel_medialists');
     await queryInterface.bulkDelete('carousels');
   }
 };
