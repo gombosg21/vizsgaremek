22d70ddabcaa5a5995e9c3d090deb955f4e214eb
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-21 13:26:09 +0100

    defined rules + filetype check

--- backend/middlewares/validators/reaction.js
+++ backend/middlewares/validators/reaction.js
@@ -1,10 +1,35 @@
 const reaction = require('../models').reaction;
+const validator = require('express-validator');
 
 exports.createRules = () => {
     return [
-        validator.body('name'),
-        validator.body('data')
+        validator.body('name').notEmpty().withMessage("name cannot be empty"),
+        validator.body('name').isAlpha().withMessage("name must be a string of characters only"),
+        validator.body('name').isLength({min:3,max:50}).withMessage("name be least 3 characters and most 50 long"),
+        validator.body('data').notEmpty().withMessage("data cannot be empty"),
     ];
+};
+
+exports.validateNewReactionData = () => {
+    const reactionData = req.file;
+
+    try {
+        if (!reactionData) {
+            return res.status(400)
+                .json({ "error": "no file given" });
+        };
+        var type = filetype.filetypemime(reactionData);
+
+        if (type[0].match(/image/gi)) {
+            return next();
+        } else {
+            return res.status(400)
+                .json({ "error": "invalid file format, must be type of image." });
+        };
+    } catch (error) {
+        console.error(error);
+        return res.status(500);
+    };
 };
 
 exports.reactionCheckIfNameConflicts = async (req, res, next) => {
