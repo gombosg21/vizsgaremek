b88ca7fa15f0d689fb802d48b2b05a301e2a3aa4
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-21 19:14:59 +0100

    added test credential wipe

--- backend/util/destroy-db.js
+++ backend/util/destroy-db.js
@@ -1,9 +1,15 @@
 const db = require('./db');
+const fs = require('fs');
 const process = require('process');
 
 db.connect();
 db.destroyDB();
 db.disconnect();
+
+fs.unlink('test_accounts_credentials.json', (err) => {
+    if (err) { throw err }
+    console.log("test account credentials destoryed");
+});
 
 process.on('exit', (code) => {
     console.log(`Program has exited with code: ${code}`)
