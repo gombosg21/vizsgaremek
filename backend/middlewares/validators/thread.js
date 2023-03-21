6659f782745139103c59452c5156500702df4223
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-21 13:26:44 +0100

    added abort messages

--- backend/middlewares/validators/thread.js
+++ backend/middlewares/validators/thread.js
@@ -10,14 +10,14 @@ exports.createThreadRules = () => {
 };
 
 exports.editThreadRules = () => {
-    return [validator.oneOf([
-        validator.body("name").exists(),
-        validator.body("status").exists()
-    ])
-        ,
-    validator.body("name").optional({ nullable: true, checkFalsy: true }).isAscii().withMessage("only letters and numbers and simple characters are allowed"),
-    validator.body("name").optional({ nullable: true, checkFalsy: true }).isLength({ max: 100 }).withMessage("thread name must be max 100 characters long"),
-    validator.body("status").optional({ nullable: true, checkFalsy: true }).isNumeric().withMessage("thread status must be a number")
+    return [
+        validator.oneOf([
+            validator.body("name").exists(),
+            validator.body("status").exists()
+        ], "no changes, aborting" ),
+        validator.body("name").optional({ nullable: true, checkFalsy: true }).isAscii().withMessage("only letters and numbers and simple characters are allowed"),
+        validator.body("name").optional({ nullable: true, checkFalsy: true }).isLength({ max: 100 }).withMessage("thread name must be max 100 characters long"),
+        validator.body("status").optional({ nullable: true, checkFalsy: true }).isNumeric().withMessage("thread status must be a number")
     ]
 };
 
@@ -32,13 +32,13 @@ exports.searchThreadRules = () => {
             validator.query('creater_id').exists(),
             validator.query('parent').exists(),
             validator.query('content').exists()
-        ], "empty query, aborting"),
+        ], "empty query, aborting" ),
         validator.query('name').optional({ nullable: true, checkFalsy: true }).isAlpha().withMessage('name can only be a string of letters'),
         validator.query('created_start').optional({ nullable: true, checkFalsy: true }).isDate().withMessage('created_start must be a valid date'),
         validator.query('created_end').optional({ nullable: true, checkFalsy: true }).isDate().withMessage('created_end must be a valid date'),
         validator.query('activity_start').optional({ nullable: true, checkFalsy: true }).isDate().withMessage('activity_start must be a valid date'),
         validator.query('activity_end').optional({ nullable: true, checkFalsy: true }).isDate().withMessage('activity_end must be a valid date'),
-        validator.query('creater_id').optional({ nullable: true, checkFalsy: true }).isNumeric({min:1}).withMessage('creater_id must be a whole number, abowe 0'),
+        validator.query('creater_id').optional({ nullable: true, checkFalsy: true }).isNumeric({ min: 1 }).withMessage('creater_id must be a whole number, abowe 0'),
         validator.query('parent').optional({ nullable: true, checkFalsy: true }).isAlpha().withMessage('parent type can only be a string of letters'),
         validator.query('content').optional({ nullable: true, checkFalsy: true }).isAscii().withMessage('content can only be a letters numbers and special characters')
     ]
@@ -57,6 +57,6 @@ exports.checkIfThreadExsits = async (req, res, next) => {
         };
     } catch (error) {
         console.error(error);
-        res.status(500);
+        return res.status(500);
     };
 };
