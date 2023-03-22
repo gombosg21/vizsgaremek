82f77a5dfe219306fc87146c8c3254b5589453b8
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-22 20:02:28 +0100

    fixed DB request

--- backend/middlewares/validators/story.js
+++ backend/middlewares/validators/story.js
@@ -4,7 +4,7 @@ const carousel = require('../../models').carousel;
 exports.checkIfStoryIDExsits = async (req, res, next) => {
     const ID = req.params.storyID;
     try {
-        const story = await carousel.findByPK(ID);
+        const story = await carousel.findByPk(ID);
         if (!story) {
             return res.status(404).json({ "error": `carousel with id : ${ID} does not exsits` });
         } else {
