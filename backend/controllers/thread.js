6d100e2f7f5b3bd9492d90a5a5404f5571ef6dfa
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-22 11:22:18 +0100

    fixed query

--- backend/controllers/thread.js
+++ backend/controllers/thread.js
@@ -2,6 +2,7 @@ const thread = require("../models").thread;
 const comment = require("../models").comment;
 const user = require("../models").user;
 const media = require("../models").media;
+const carousel = require("../models").carousel;
 const { Op } = require("sequelize");
 // const story = require("../models").story;
 
@@ -146,19 +147,21 @@ exports.searchThreads = async (req, res, next) => {
         const year = date.getFullYear();
         const currDate = new Date(year, month, day).toJSON();
 
-        const threadName = req.query.name ?? "";
-        const createdStart = req.query.created_start ?? "0000-00-00";
+        const threadName = req.query.name;
+        const createdStart = req.query.created_start ?? "1000-01-01";
         const createdEnd = req.query.created_end ?? currDate;
-        const lastActivityStart = req.query.activity_start ?? "0000-00-00";
+        const lastActivityStart = req.query.activity_start ?? "1000-01-01";
         const lastActivityEnd = req.query.activity_end ?? currDate;
-        const createrID = req.params.creater_id;
+        const createrID = req.query.creater_id;
         const parentType = req.query.parent;
         const threadContent = req.query.content;
+
+        //console.log(parentType)
+        //console.log(createrID)
 
         const query = {
             where:
             {
-                name: threadName,
                 created: { [Op.between]: [createdStart, createdEnd] },
                 last_activity: { [Op.between]: [lastActivityStart, lastActivityEnd] }
             },
@@ -170,44 +173,50 @@ exports.searchThreads = async (req, res, next) => {
                 }]
         };
 
+        if (threadName) {
+            query.where.name = { [Op.substring]: threadName }
+        };
+
         if (createrID) {
             query.where.user_ID = createrID;
         };
         if (threadContent) {
-            query.include.push({ model: comment, attributes: ['ID', 'content'] });
-            query.where.comment = { [Op.substring]: threadContent };
-        };
-
-        switch (parentType) {
-            case ("user"): {
-                query.where.profile_ID = { [Op.not]: null };
-                break;
-            }
-            case ("media"): {
-                query.where.media_ID = { [Op.not]: null };
-                break;
-            }
-            case ("story"): {
-                query.where.story_ID = { [Op.not]: null };
-                break;
-            }
-            case (undefined || null || ""): {
-                // any parent is also a valid type
-                break;
-            }
-            default: {
-                return res.status(400).json({ error: "bad search parent type" });
-            }
-        };
-
-        console.log(query);
-
-        const threadList = await thread.findAll(query);
-
-        return res.status(200).json({ results: threadList })
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500);
+            query.include.push({ model: comment, where:{ content: { [Op.substring]: threadContent } }, attributes: ['ID', 'content'] });
     };
+
+    switch (parentType) {
+        case ("profile"): {
+            query.where.profile_ID = { [Op.not]: null };
+            query.include.push({ model: user, attributes: ['ID', 'alias'] });
+            break;
+        }
+        case ("media"): {
+            query.where.media_ID = { [Op.not]: null };
+            query.include.push({ model: media, attributes: ['ID', 'user_ID'] });
+            break;
+        }
+        case ("story"): {
+            query.where.carousel_ID = { [Op.not]: null };
+            query.include.push({ model: carousel, attributes: ['ID', 'user_ID'] });
+            break;
+        }
+        case (undefined): {
+            // any parent is also a valid type
+            break;
+        }
+        default: {
+            return res.status(400).json({ error: "bad search parent type" });
+        }
+    };
+
+    console.log(query);
+
+    const threadList = await thread.findAll(query);
+
+    return res.status(200).json({ results: threadList })
+
+} catch (error) {
+    console.error(error);
+    return res.status(500);
+};
 };
