abdacbefbd06ae60a5ea13782e5e7e35c4e51e74
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-24 16:51:36 +0100

    fixed wonky logic

--- backend/helpers/seeding/array.js
+++ backend/helpers/seeding/array.js
@@ -3,9 +3,9 @@ const getRandomArrayMember = (array) => {
     if (!array) { throw new Error("argumnet array missing") };
     if (!(array instanceof Array)) { throw new TypeError("array must be an array") };
     if (array.length == 0) { throw new Error("array cannot be an empty array") };
-    if (array.lenght == 1) { throw new Error("cannot random select from a 1 lenght array") };
+    if (array.length == 1) { throw new Error("cannot random select from a 1 length array") };
 
-    const randomIndex = Math.floor(array.lenght * Math.random());
+    const randomIndex = Math.floor(array.length * Math.random());
     return array[randomIndex];
 };
 
@@ -13,17 +13,19 @@ const getRandomArraySlice = (array) => {
     if (!array) { throw new Error("argumnet array missing") };
     if (!(array instanceof Array)) { throw new TypeError("array must be an array") };
     if (array.length == 0) { throw new Error("array cannot be an empty array") };
-    if (array.lenght == 1) { throw new Error("cannot random slice from a 1 lenght array") };
+    if (array.length == 1) { throw new Error("cannot random slice from a 1 length array") };
 
     let randomNum1 = 0;
     let randomNum2 = 0;
 
-    while (randomNum1 >= randomNum2) {
-        randomNum1 = Math.floor(array.lenght * Math.random());
-        randomNum2 = Math.floor(array.lenght * Math.random());
-    };
+    
 
+    while ((randomNum1 + 1) >= randomNum2) {
+        randomNum1 = Math.floor(array.length * Math.random());
+        randomNum2 = Math.floor(array.length * Math.random());
+    };
     const newArray = array.slice(randomNum1, randomNum2);
+
     return newArray;
 };
 
@@ -31,7 +33,7 @@ const getMixedArray = (array) => {
     if (!array) { throw new Error("argumnet array missing") };
     if (!(array instanceof Array)) { throw new TypeError("array must be an array") };
     if (array.length == 0) { throw new Error("array cannot be an empty array") };
-    if (array.lenght == 1) { throw new Error("cannot random mix a 1 lenght array") };
+    if (array.length == 1) { throw new Error("cannot random mix a 1 length array") };
 
     var shuffledArray = array;
     for (let i = array.length - 1; i > 0; i--) {
@@ -48,8 +50,9 @@ const getRandomMixedArraySlice = (array) => {
     if (!array) { throw new Error("argumnet array missing") };
     if (!(array instanceof Array)) { throw new TypeError("array must be an array") };
     if (array.length == 0) { throw new Error("array cannot be an empty array") };
-    if (array.lenght == 1) { throw new Error("cannot generate a mixed random slice from a 1 lenght array") };
+    if (array.length == 1) { throw new Error("cannot generate a mixed random slice from a 1 length array") };
 
+    
     const shuffledArray = getMixedArray(array);
 
     const randomSliceFromShuffled = getRandomArraySlice(shuffledArray);
