a96a77b0c4de20811847b4984c996f5417aa59ad
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-24 13:49:02 +0100

    added validation

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const mediaController = require('../controllers/media');
-const mediaValidator = require('../middlewares/validators/media')
+const mediaValidator = require('../middlewares/validators/media');
+const validate = require("../middlewares/validators/common").validate;
 const userValidator = require('../middlewares/validators/user');
 const auth = require('../middlewares/authentiaction/auth');
 const ownership = require('../middlewares/authentiaction/ownership');
@@ -9,13 +10,13 @@ const upload = multer({ storage: new multer.memoryStorage() });
 
 
 // create media with user_ID
-router.post('/media', auth.isAuth, upload.single('image'), mediaController.uploadMedia);
+router.post('/media', auth.isAuth, upload.single('image'), mediaValidator.uploadRules(), validate, mediaController.uploadMedia);
 
 // edit media with media_ID
 // delete media with media_ID
 // view media with media_ID
 router.route('/media/:mediaID')
-    .patch(auth.isAuth, ownership.isMyMedia, mediaValidator.checkIfMediaIDExsist, mediaController.editMedia)
+    .patch(auth.isAuth, ownership.isMyMedia, mediaValidator.checkIfMediaIDExsist, mediaValidator.editRules(), validate, mediaController.editMedia)
     .delete(auth.isAuth, ownership.isMyMedia, mediaValidator.checkIfMediaIDExsist, mediaController.deleteMedia)
     .get(mediaValidator.checkIfMediaIDExsist, mediaController.getMediaByID);
 
@@ -26,6 +27,6 @@ router.get('/media/all/:userID', userValidator.checkIfUserIDExsits, mediaControl
 router.get('/media/search/tags', mediaController.getAllMediaByTags);
 
 // edit tags
-router.patch('/media/:mediaID/tags', auth.isAuth, mediaController.editMediaTags);
+router.patch('/media/:mediaID/tags', auth.isAuth, mediaValidator.editTagRules(), validate, mediaController.editMediaTags);
 
 module.exports = router;
