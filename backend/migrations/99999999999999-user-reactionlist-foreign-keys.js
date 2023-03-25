003bf040a0b7275659d91532d5b5417fa89c709e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-25 16:15:11 +0100

    fixed wrong table reference

--- backend/migrations/99999999999999-user-reactionlist-foreign-keys.js
+++ backend/migrations/99999999999999-user-reactionlist-foreign-keys.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        queryInterface.addConstraint('user_reactionlists', { fields: ['profile_ID'], type: 'foreign key', name: 'fk_user_reactionlist_profile', references: { table: 'threads', field: 'ID' } });
+        queryInterface.addConstraint('user_reactionlists', { fields: ['profile_ID'], type: 'foreign key', name: 'fk_user_reactionlist_profile', references: { table: 'users', field: 'ID' } });
         queryInterface.addConstraint('user_reactionlists', { fields: ['user_ID'], type: 'foreign key', name: 'fk_user_reactionlist_user', references: { table: 'users', field: 'ID' } });
         queryInterface.addConstraint('user_reactionlists', { fields: ['reaction_ID'], type: 'foreign key', name: 'fk_user_reactionlist_reaction', references: { table: 'reactions', field: 'ID' } });
     },
