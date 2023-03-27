bcefac89d4e96aa6c48889964cca25511148a07e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 17:37:54 +0200

    better naming

--- backend/migrations/99999999999999-thread-checks.js
+++ backend/migrations/99999999999999-thread-checks.js
@@ -3,9 +3,9 @@ const { Op } = require('sequelize');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        queryInterface.addConstraint('threads', { fields: ['user_ID'], type: 'check', name: 'fk_thread_key_check', where: { [Op.or]: [{ 'profile_ID': { [Op.not]: null } }, { 'media_ID': { [Op.not]: null } }, { 'carousel_ID': { [Op.not]: null } }] } });
+        queryInterface.addConstraint('threads', { fields: ['user_ID'], type: 'check', name: 'thread_profile_or_carousel_or_media_notnull_only_check', where: { [Op.or]: [{ 'profile_ID': { [Op.not]: null } }, { 'media_ID': { [Op.not]: null } }, { 'carousel_ID': { [Op.not]: null } }] } });
     },
     async down(queryInterface, Sequelize) {
-        queryInterface.removeConstraint('threads', 'fk_thread_key_check');
+        queryInterface.removeConstraint('threads', 'thread_profile_or_carousel_or_media_notnull_only_check');
     }
 };
