306858f946df99a0d82d2a07a1ef44f9cdcbfc81
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 18:11:33 +0200

    added misisng relations

--- backend/models/profile.js
+++ backend/models/profile.js
@@ -7,11 +7,12 @@ module.exports = (sequelize, DataTypes) => {
         static associate(models) {
             this.hasOne(models.user, { sourceKey: "user_ID", foreignKey: "ID" });
             this.hasOne(models.media, { sourceKey: "picture_ID", foreignKey: "ID" });
-            this.belongsToMany(models.reaction, { through: models.user_reactionlist, foreignKey: "profile_ID", sourceKey: "user_ID"});
+            this.hasOne(models.thread, { sourceKey: "user_ID", foreignKey: "profile_ID" });
+            this.belongsToMany(models.reaction, { through: models.user_reactionlist, foreignKey: "profile_ID", sourceKey: "user_ID" });
         };
     };
     profile.init({
-        user_ID: { type: DataTypes.INTEGER, allowNull: false, primaryKey:true ,unique: true, references: { model: 'user', key: 'ID' } },
+        user_ID: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, unique: true, references: { model: 'user', key: 'ID' } },
         description: { type: DataTypes.TEXT, allowNull: true },
         visibility: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
         picture_ID: { type: DataTypes.INTEGER, allowNull: true, references: { model: 'media', key: 'ID' } }
