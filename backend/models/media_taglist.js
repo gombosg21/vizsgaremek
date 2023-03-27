d3c887133853571bdcec95e798b7444142d54f31
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 15:25:30 +0200

    removed placholder comment

--- backend/models/media_taglist.js
+++ backend/models/media_taglist.js
@@ -4,34 +4,19 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class media_taglist extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      this.hasMany(models.tag,{sourceKey:"tag_ID",foreignKey:"ID"}),
-      this.hasMany(models.media,{sourceKey:"media_ID",foreignKey:"ID"})
+      this.hasMany(models.tag, { sourceKey: "tag_ID", foreignKey: "ID" });
+      this.hasMany(models.media, { sourceKey: "media_ID", foreignKey: "ID" });
     }
   }
   media_taglist.init({
-    tag_ID: 
+    tag_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'tag', key: 'ID' } },
+    media_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'media', key: 'ID' } }
+  },
     {
-      allowNull:false,
-      type:DataTypes.INTEGER,
-      references:{model:'tag',key:'ID'}
-    },
-    media_ID: 
-    {
-      allowNull:false,
-      type:DataTypes.INTEGER,
-      references:{model:'media',key:'ID'}
-    }
-  }, 
-  {
-    sequelize,
-    modelName: 'media_taglist',
-    timestamps: false
-  });
+      sequelize,
+      modelName: 'media_taglist',
+      timestamps: false
+    });
   return media_taglist;
 };
