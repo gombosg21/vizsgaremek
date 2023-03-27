9de15c039d9ca93c26d342f5cb4a67f189f6d8cf
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 17:39:47 +0200

    fixed relations

--- backend/models/thread_reactionlist.js
+++ backend/models/thread_reactionlist.js
@@ -6,13 +6,13 @@ module.exports = (sequelize, DataTypes) => {
   class thread_reactionlist extends Model {
     static associate(models) {
       this.hasMany(models.user, { sourceKey: "user_ID", foreignKey: "ID" });
-      this.hasMany(models.carousel, { sourceKey: "thread_ID", foreignKey: "ID" });
+      this.hasMany(models.thread, { sourceKey: "thread_ID", foreignKey: "ID" });
       this.hasMany(models.reaction, { sourceKey: "reaction_ID", foreignKey: "ID" });
     }
   }
   thread_reactionlist.init({
     user_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'user', key: 'ID' } },
-    thread_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'thread_ID', key: 'ID' } },
+    thread_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'thread', key: 'ID' } },
     reaction_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'reaction', key: 'ID' } },
     date: { allowNull: false, type: DataTypes.DATE, defaultValue: DataTypes.NOW }
   },
--- backend/models/user_reactionlist.js
+++ backend/models/user_reactionlist.js
@@ -5,9 +5,9 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class user_reactionlist extends Model {
     static associate(models) {
-      this.hasMany(models.user, { sourceKey: "user_ID", foreignKey: "ID" });
-      this.hasMany(models.user, { sourceKey: "profile_ID", foreignKey: "ID" });
-      this.hasMany(models.reaction, { sourceKey: "reaction_ID", foreignKey: "ID" });
+      this.hasMany(models.user, { sourceKey: "user_ID", foreignKey: "ID", as: "profile_reaction_owner" });
+      this.hasMany(models.profile, { sourceKey: "profile_ID", foreignKey: "user_ID", as: "profile_reaction_subject" });
+      this.hasMany(models.reaction, { sourceKey: "reaction_ID", foreignKey: "ID", as: "profile_reaction" });
     }
   }
   user_reactionlist.init({
@@ -15,14 +15,13 @@ module.exports = (sequelize, DataTypes) => {
     profile_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'user', key: 'ID' } },
     reaction_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'reaction', key: 'ID' } },
     date: { allowNull: false, type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-  },
-    {
-      sequelize,
-      modelName: 'user_reactionlist',
-      timestamps: true,
-      createdAt: 'date',
-      updatedAt: false,
-      paranoid: false,
-    });
+  }, {
+    sequelize,
+    modelName: 'user_reactionlist',
+    timestamps: true,
+    createdAt: 'date',
+    updatedAt: false,
+    paranoid: false,
+  });
   return user_reactionlist;
 };
