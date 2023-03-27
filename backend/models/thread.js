e070c8dca492bbcfc92e81e0d3bd3397b84e98a4
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 18:11:45 +0200

    added missing relations

--- backend/models/thread.js
+++ backend/models/thread.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.belongsTo(models.user, { foreignKey: "user_ID" });
+      this.belongsTo(models.profile, { foreignKey: "profile_ID" });
       this.belongsTo(models.media, { foreignKey: "media_ID" });
       this.belongsTo(models.carousel, { foreignKey: "carousel_ID" });
       this.hasMany(models.comment, { foreignKey: "thread_ID" });
