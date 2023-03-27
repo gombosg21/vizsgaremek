5fdbcb99d341906b9c824b4bdf00e75fdc2e24b7
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 15:24:39 +0200

    TODOs

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -8,6 +8,7 @@ const userValidator = require('../middlewares/validators/user');
 
 
 // create story
+// TODO verify mediaID
 router.post('/story', auth.isAuth, storyValidator.createStoryRules(), validate, storyController.createStory);
 
 //get all stories from user
@@ -15,6 +16,7 @@ router.post('/story', auth.isAuth, storyValidator.createStoryRules(), validate, 
 router.get('/story/user/:userID/all', userValidator.checkIfUserIDExsits, storyController.getAllStoryFromUser);
 
 // edit story
+// TODO verify mediaID
 // get story
 // delete story
 router.route('/story/:storyID')
