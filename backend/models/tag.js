c193840c5c52134aca37b7391f1824e200ec70cb
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-27 17:40:13 +0200

    better readability, by a tiny margin

--- backend/models/tag.js
+++ backend/models/tag.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class tag extends Model {
     static associate(models) {
-      this.belongsToMany(models.media,{through:models.media_taglist,foreignKey:"tag_ID",sourceKey:"ID"});
+      this.belongsToMany(models.media, { through: models.media_taglist, foreignKey: "tag_ID", sourceKey: "ID" });
     }
 
     rename(newName) {
@@ -14,14 +14,14 @@ module.exports = (sequelize, DataTypes) => {
 
   };
   tag.init({
-    ID: {type:DataTypes.INTEGER, autoIncrement:true,primaryKey:true,allowNull:false},
-    name: {type:DataTypes.STRING,unique: true,allowNull: false },
-    deletedAt:{type:DataTypes.DATE,allowNull:true,defaultValue:null}
+    ID: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    deletedAt: { type: DataTypes.DATE, allowNull: true, defaultValue: null }
   }, {
     sequelize,
     modelName: 'tag',
     timestamps: true,
-    updatedAt:false,
+    updatedAt: false,
     createdAt: false,
     deletedAt: true,
     paranoid: true
