3e5755198dde5115a2a8bdbc6eb3ed3d2ffefa7c
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-28 22:28:51 +0200

    no need for custom array validation...

--- backend/middlewares/validators/story.js
+++ backend/middlewares/validators/story.js
@@ -1,6 +1,8 @@
-const arrayValidator = require('../../helpers/validation/array');
 const validator = require('express-validator');
 const carousel = require('../../models').carousel;
+const media = require('../../models').media;
+
+//No async middlewares :(
 
 exports.checkIfStoryIDExsits = async (req, res, next) => {
     const ID = req.params.storyID;
@@ -18,39 +20,29 @@ exports.checkIfStoryIDExsits = async (req, res, next) => {
 };
 
 exports.createStoryRules = () => {
+    // const mediaIDs = (await media.findAll({ attributes: ['ID'] })).map(Media => Media.ID);
+    const item_number_list = [];
     return [
         validator.body('name').notEmpty().withMessage("name cannot be empty"),
         validator.body('name').isAscii().withMessage("name must be letters numbers and special characters only"),
         validator.body('description').notEmpty().withMessage('description cannot be empty'),
         validator.body('description').isAscii().withMessage('description must be letters numbers and special characters only'),
         validator.body('visibility').optional({ nullable: true, checkFalsy: true }).isNumeric({ min: 0, max: 3 }).withMessage('visibility must be a whole positive number in range of 0-3'),
-        validator.body('medias').custom((value) => { return arrayValidator.isArray(value, "medias must be an array") }),
-        validator.body('medias').custom((value) => { return arrayValidator.nonEmptyArray(value, "medias cannot be an empty array") }),
-        validator.body('medias').custom((value) => {
-            value.forEach(media => {
-                if (media.ID == undefined) { throw new Error("media ID cannot be empty") };
-                if (typeof media.ID != "number") { throw new Error("media ID must be a number"); };
-                if (media.ID < 0) { throw new Error("media ID cannot be a negative number") };
-                if (media.ID % 1 != 0) { throw new Error("media ID must be a whole number") };
-            });
-            return value;
+        validator.body('medias').isArray().withMessage("medias must be an array"),
+        validator.body('medias').isArray({ min: 1 }).withMessage("medias cannot be an empty array"),
+        validator.body('medias.*.ID').exists().withMessage("medias must have a field ID"),
+        validator.body('medias.*.ID').isNumeric().withMessage("media ID must be a number"),
+        validator.body('medias.*.ID').custom((value) => { if (value < 1 || value % 1 != 0) { throw new Error("media ID must be a positive non-zero whole number") } else { return value } }),
+        // validator.body('medias.*.ID').custom((value) => { if (!(mediaIDs.contains(value))) { throw new Error("media ID must be a positive non-zero whole number") } else { return value } }),
+        validator.body('medias.*.item_number').exists().withMessage("medias must have a field item_number"),
+        validator.body('medias.*.item_number').isNumeric().withMessage("item number must be a number"),
+        validator.body('medias.*.item_number').custom((value) => { if (value < 0 || value % 1 != 0) { throw new Error("item number must be a whole positive number") } else { return value } }),
+        validator.body('medias.*.item_number').custom((value) => {
+            if (item_number_list.contains(value)) { throw new Error("item_number must be be an unique number")} else {
+                item_number_list.push(value);
+                return value
+            };
         }),
-        validator.body('medias').custom((value) => {
-            value.forEach(media => {
-                if (media.item_number == undefined) { throw new Error("media item_number cannot be empty") };
-                if (typeof media.item_number != "number") { throw new Error("media item_number must be a number"); };
-                if (media.item_number < 0) { throw new Error("media item_number cannot be a negative number") };
-                if (media.item_number % 1 != 0) { throw new Error("media item_number must be a whole number") };
-            });
-            return value;
-        }),
-        validator.body('medias').custom((value) => {
-            value.forEach(media => {
-                if (media.item_description) {
-                    if (typeof media.item_description != "string") { throw new Error("media item_description must be a string"); };
-                };
-            });
-            return value;
-        }),
+        validator.body('medias.*.item_description').optional({nullable:true,checkFalsy:true}).isAscii().withMessage("item description can only be made of letters numbers and special characters")
     ];
 };
