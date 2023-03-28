26a7cf1ce4c8ef1f502d9d1d4cd45541245d930e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-28 10:56:34 +0200

    naming

--- backend/models/profile_reactionlist.js
+++ backend/models/profile_reactionlist.js
@@ -3,25 +3,25 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user_reactionlist extends Model {
+  class profile_reactionlist extends Model {
     static associate(models) {
       this.hasMany(models.user, { sourceKey: "user_ID", foreignKey: "ID"});
       this.hasMany(models.profile, { sourceKey: "profile_ID", foreignKey: "user_ID"});
       this.hasMany(models.reaction, { sourceKey: "reaction_ID", foreignKey: "ID"});
     }
   }
-  user_reactionlist.init({
+  profile_reactionlist.init({
     user_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'user', key: 'ID' } },
     profile_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'user', key: 'ID' } },
     reaction_ID: { allowNull: false, type: DataTypes.INTEGER, references: { model: 'reaction', key: 'ID' } },
     date: { allowNull: false, type: DataTypes.DATE, defaultValue: DataTypes.NOW }
   }, {
     sequelize,
-    modelName: 'user_reactionlist',
+    modelName: 'profile_reactionlist',
     timestamps: true,
     createdAt: 'date',
     updatedAt: false,
     paranoid: false,
   });
-  return user_reactionlist;
+  return profile_reactionlist;
 };
