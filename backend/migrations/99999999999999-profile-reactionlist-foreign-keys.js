0d783fb57c3ccdb765c52a8dd2e4a915c594df03
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-30 20:49:58 +0200

    better naming

--- backend/migrations/99999999999999-profile-reactionlist-foreign-keys.js
+++ backend/migrations/99999999999999-profile-reactionlist-foreign-keys.js
@@ -2,13 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        queryInterface.addConstraint('profile_reactionlists', { fields: ['profile_ID'], type: 'foreign key', name: 'fk_user_reactionlist_profile', references: { table: 'users', field: 'ID' } });
-        queryInterface.addConstraint('profile_reactionlists', { fields: ['user_ID'], type: 'foreign key', name: 'fk_user_reactionlist_user', references: { table: 'users', field: 'ID' } });
-        queryInterface.addConstraint('profile_reactionlists', { fields: ['reaction_ID'], type: 'foreign key', name: 'fk_user_reactionlist_reaction', references: { table: 'reactions', field: 'ID' } });
+        queryInterface.addConstraint('profile_reactionlists', { fields: ['profile_ID'], type: 'foreign key', name: 'fk_profile_reactionlist_profile', references: { table: 'users', field: 'ID' } });
+        queryInterface.addConstraint('profile_reactionlists', { fields: ['user_ID'], type: 'foreign key', name: 'fk_profile_reactionlist_user', references: { table: 'users', field: 'ID' } });
+        queryInterface.addConstraint('profile_reactionlists', { fields: ['reaction_ID'], type: 'foreign key', name: 'fk_profile_reactionlist_reaction', references: { table: 'reactions', field: 'ID' } });
     },
     async down(queryInterface, Sequelize) {
-        queryInterface.removeConstraint('profile_reactionlists', 'fk_user_reactionlist_profile');
-        queryInterface.removeConstraint('profile_reactionlists', 'fk_user_reactionlist_user');
-        queryInterface.removeConstraint('profile_reactionlists', 'fk_user_reactionlist_reaction');
+        queryInterface.removeConstraint('profile_reactionlists', 'fk_profile_reactionlist_profile');
+        queryInterface.removeConstraint('profile_reactionlists', 'fk_profile_reactionlist_user');
+        queryInterface.removeConstraint('profile_reactionlists', 'fk_profile_reactionlist_reaction');
     }
 };
