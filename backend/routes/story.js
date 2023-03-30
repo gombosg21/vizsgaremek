d707f049faae934500051a7b0151d03ab2b3dfa0
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-03-30 13:41:29 +0200

    added story search

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -8,21 +8,21 @@ const userValidator = require('../middlewares/validators/user');
 
 
 // create story
-// TODO verify mediaID
 router.post('/story', auth.isAuth, storyValidator.createStoryRules(), validate, storyController.createStory);
 
 //get all stories from user
-
 router.get('/story/user/:userID/all', userValidator.checkIfUserIDExsits, storyController.getAllStoryFromUser);
 
 // edit story
-// TODO verify mediaID
 // get story
 // delete story
 router.route('/story/:storyID')
     .get(storyValidator.checkIfStoryIDExsits, storyController.getStory)
-    .patch(storyValidator.checkIfStoryIDExsits, auth.isAuth, ownership.isMyStory, storyController.editStory)
+    .patch(storyValidator.checkIfStoryIDExsits, auth.isAuth, ownership.isMyStory, storyValidator.editStoryRules(), validate, storyController.editStory)
     .delete(storyValidator.checkIfStoryIDExsits, auth.isAuth, ownership.isMyStory, storyController.deleteStory);
+
+//search story by query params
+router.get('/story', storyValidator.searchStoryRules(), validate, storyController.searchStory);
 
 
 module.exports = router;
