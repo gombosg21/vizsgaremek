14df3ad95bf375082c307721c8e5d7f2fc5e6b94
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-05 21:18:48 +0200

    finished logic

--- backend/seeders/19-add-friends.js
+++ backend/seeders/19-add-friends.js
@@ -12,26 +12,35 @@ module.exports = {
         const userIDs = (await user.findAll({ attributes: ['ID'] })).map(User => User.ID);
 
         userIDs.forEach(ID => {
-            var userFriendIDs = getRandomMixedArraySlice(userIDs);
+            var userFriendIDs = getRandomSubArray(userIDs);
             var userFriends = [];
 
             for (let i = 0; i < userFriendIDs.length; i++) {
                 if (userFriendIDs[i] == ID) {
-                    userFriendIDs.splice(i,1);
+                    userFriendIDs.splice(i, 1);
                 };
                 userFriends.push({
                     user_ID: ID,
                     friend_ID: userFriendIDs[i],
-                    pending : Math.round(Math.random()) = 1 ? true : false,
-                    date: randomDate("2000-01-01","2022-12-31")
+                    pending: Math.round(Math.random()) = 1 ? true : false,
+                    date: randomDate("2000-01-01", "2022-12-31"),
+                    unikey: ID > userFriendIDs[i] ? ID + "_" + userFriendIDs[i] : userFriendIDs[i] + "_" + ID
                 });
             };
             friends.concat(userFriends);
         });
 
-        for (let i = 0; i < friends.length; i++) {
-            
+        friends.sort((a, b) => ((a.unikey > b.unikey) ? 1 : ((b.unikey > a.unikey) ? -1 : 0)));
+
+        for (let i = 0; i < (friends.length - 1); i++) {
+            if (friends[i].unikey == friends[i + 1].unikey) {
+                friends.splice(i, 1);
+            };
         };
+
+        friends.forEach(Friend => {
+            delete Friend.unikey;
+        });
 
         await queryInterface.bulkInsert('friends', friends);
     },
