83ddb2926e84aeec8b1820e5209f55fce8fdfe6c
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-05 19:29:11 +0200

    moved, added new check

--- backend/migrations/999999999999992-friends-checks.js
+++ backend/migrations/999999999999992-friends-checks.js
@@ -4,8 +4,10 @@ const { Op, col } = require('sequelize');
 module.exports = {
     async up(queryInterface, Sequelize) {
         queryInterface.addConstraint('friends', { fields: ['user_ID', 'friend_ID'], type: 'check', where: { 'user_ID': { [Op.ne]: col('friend_ID') } }, name: 'friends_no_self_friend' });
+        queryInterface.addConstraint('friends', { fields: ['friends_unique_computed_key'], type: 'unique', name: 'firends_no_repeat' })
     },
     async down(queryInterface, Sequelize) {
         queryInterface.removeConstraint('friends', 'friends_no_self_friend');
+        queryInterface.removeConstraint('friends', 'firends_no_repeat');
     }
 };
