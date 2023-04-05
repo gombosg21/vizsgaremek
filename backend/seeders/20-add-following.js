7fead08c54c775af94e935e83ae13edadcb05dbe
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-05 21:26:10 +0200

    defined logic

--- backend/seeders/20-add-following.js
+++ backend/seeders/20-add-following.js
@@ -7,7 +7,27 @@ const randomDate = require('../helpers/seeding/date').getRandomDate;
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        //await queryInterface.bulkInsert('followed', threadReactions);
+        const followings = [];
+
+        const userIDs = (await user.findAll({ attributes: ['ID'] })).map(User => User.ID);
+
+        userIDs.forEach(ID => {
+            var followedIDs = getRandomSubArray(userIDs);
+            var followeds = [];
+            for (let i = 0; i < followedIDs; i++) {
+                if (followedIDs[i] == ID) {
+                    followedIDs.splice(i, 1);
+                };
+                followeds.push({
+                    user_ID: ID,
+                    followed_ID: followedIDs[i],
+                    date: randomDate("2000-01-01", "2022-12-31")
+                });
+            };
+            followings.concat(followeds);
+        });
+
+        await queryInterface.bulkInsert('followed', followings);
     },
     async down(queryInterface, Sequelize) {
         await queryInterface.bulkDelete('followed');
