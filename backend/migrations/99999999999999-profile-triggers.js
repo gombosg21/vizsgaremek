34153c7e6d205b6b83e2caa4b4b36d35c4e888c6
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-06 16:26:34 +0200

    fixed for real

--- backend/migrations/99999999999999-profile-triggers.js
+++ backend/migrations/99999999999999-profile-triggers.js
@@ -1,47 +1,45 @@
 'use strict';
+const db = require("../util/db");
+const config = require("../config/config.json");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        Sequelize.literal(
-            'DELIMITER // ' +
+        (await db.connection).query(
             'CREATE TRIGGER UPDATE_PROFILE_ALIAS_AFTER_CREATE_IF_NULL_TO_USER_NAME ' +
             'AFTER INSERT ON profiles ' +
             'FOR EACH ROW ' +
             'BEGIN ' +
             'DECLARE USERNAME VARCHAR(255); ' +
             'BEGIN ' +
-            'IF profiles.alias = NULL' +
+            'IF profiles.alias = NULL ' +
             'THEN ' +
             'SELECT name INTO USERNAME FROM users WHERE user.ID = new.profile.user_ID; ' +
             'UPDATE profiles ' +
             'SET alias = USERNAME WHERE user_ID = new.user_ID; ' +
             'END IF; ' +
             'END; ' +
-            'END; ' +
-            '//'
+            'END; '
         );
-        Sequelize.literal(
-            'DELIMITER // ' +
+        (await db.connection).query(
             'CREATE TRIGGER UPDATE_PROFILE_AFTER_UPDATE_ALIAS_IF_NULL_TO_USER_NAME ' +
             'AFTER UPDATE ON profiles ' +
             'FOR EACH ROW ' +
             'BEGIN ' +
             'DECLARE USERNAME VARCHAR(255); ' +
             'BEGIN ' +
-            'IF profiles.alias = NULL' +
+            'IF profiles.alias = NULL ' +
             'THEN ' +
             'SELECT name INTO USERNAME FROM users WHERE user.ID = new.profile.user_ID; ' +
             'UPDATE profiles ' +
             'SET alias = USERNAME WHERE user_ID = new.user_ID; ' +
             'END IF; ' +
             'END; ' +
-            'END; ' +
-            '//'
+            'END; '
         );
     },
 
     async down(queryInterface, Sequelize) {
-        Sequelize.literal('DROP TRIGGER UPDATE_PROFILE_ALIAS_AFTER_CREATE_IF_NULL_TO_USER_NAME');
-        Sequelize.literal('DROP TRIGGER UPDATE_PROFILE_AFTER_UPDATE_ALIAS_IF_NULL_TO_USER_NAME');
+        (await db.connection).query('DROP TRIGGER UPDATE_PROFILE_ALIAS_AFTER_CREATE_IF_NULL_TO_USER_NAME');
+        (await db.connection).query('DROP TRIGGER UPDATE_PROFILE_AFTER_UPDATE_ALIAS_IF_NULL_TO_USER_NAME');
     }
 };
--- backend/migrations/999999999999991-friends-computed-fields.js
+++ backend/migrations/999999999999991-friends-computed-fields.js
@@ -1,9 +1,9 @@
 'use strict';
-const sequelize = require('sequelize');
+const db = require("../util/db");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        Sequelize.literal("ALTER TABLE friends ADD friends_unique_computed_key varchar(255) GENERATED ALWAYS AS ( CONCAT( LEAST( friends.user_ID, friends.friend_ID ) , '_' , GREATEST( friends.user_ID , friends.friend_ID ))) STORED; ");
+        (await db.connection).query("ALTER TABLE friends ADD friends_unique_computed_key varchar(255) GENERATED ALWAYS AS ( CONCAT( LEAST( friends.user_ID, friends.friend_ID ) , '_' , GREATEST( friends.user_ID , friends.friend_ID ))) STORED; ");
     },
     async down(queryInterface, Sequelize) {
         //
