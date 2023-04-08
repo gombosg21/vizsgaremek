d648bbb0bb6bbd6e57ab3bf1c7060dc457877d16
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-08 14:18:10 +0200

    added more validation

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,6 +1,7 @@
 const friendController = require("../controllers/friends");
 const auth = require("../middlewares/authentiaction/auth");
 const userValidator = require("../middlewares/validators/user");
+const friendValidator = require("../middlewares/validators/friend");
 const router = require('express').Router();
 
 // get all pending friends
@@ -10,13 +11,13 @@ router.get("/friends/pending", auth.isAuth, friendController.getPendingFriends);
 router.get("/friends/:userID", userValidator.checkIfUserIDExsits, friendController.getFriends);
 
 // add friend
-router.post("/friends/:userID/add", auth.isAuth, userValidator.checkIfUserIDExsits, friendController.requestFriend);
+router.post("/friends/:userID/add", auth.isAuth, userValidator.checkIfUserIDExsits, friendValidator.checkIfNotSelf, friendController.requestFriend);
 
 // confirm friend
-router.post("/friends/:userID/confirm", auth.isAuth, userValidator.checkIfUserIDExsits, friendController.verifyFriend);
+router.post("/friends/:userID/confirm", auth.isAuth, userValidator.checkIfUserIDExsits, friendValidator.checkIfNotSelf, friendController.verifyFriend);
 
 // remove friend
-router.delete("/friends/:userID", auth.isAuth, userValidator.checkIfUserIDExsits, friendController.removeFrined);
+router.delete("/friends/:userID", auth.isAuth, userValidator.checkIfUserIDExsits, friendValidator.checkIfNotSelf, friendController.removeFrined);
 
 
 module.exports = router;
