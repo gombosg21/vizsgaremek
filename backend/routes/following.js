0fa30ca77ff857edf41ae125cbbecd3711996fbe
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-10 12:40:22 +0200

    defined logic

--- backend/routes/following.js
+++ backend/routes/following.js
@@ -1,11 +1,19 @@
-// get all followed
+const router = require('express').Router();
+const auth = require("../middlewares/authentiaction/auth");
+const userValidator = require("../middlewares/validators/user");
+const followedController = require("../controllers/following");
 
-// get all followed count
-
-// unsub
-
-// sub
-
-// view followed ID
 
 // get follower count
+router.get('following/count/:userID', userValidator.checkIfUserIDExsits, followedController.getFollowerCount);
+
+// get all followed
+router.get('following/all', auth.isAuth, followedController.getFollowedList);
+
+// unsub
+router.delete('following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.unSub);
+
+// sub
+router.post('following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.sub);
+
+module.exports = router;
