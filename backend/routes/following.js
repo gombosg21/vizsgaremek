94e594f7f9bd8a30603296e280e8008b009ed2e2
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-10 12:42:12 +0200

    missing "/"

--- backend/routes/following.js
+++ backend/routes/following.js
@@ -5,15 +5,15 @@ const followedController = require("../controllers/following");
 
 
 // get follower count
-router.get('following/count/:userID', userValidator.checkIfUserIDExsits, followedController.getFollowerCount);
+router.get('/following/count/:userID', userValidator.checkIfUserIDExsits, followedController.getFollowerCount);
 
 // get all followed
-router.get('following/all', auth.isAuth, followedController.getFollowedList);
+router.get('/following/all', auth.isAuth, followedController.getFollowedList);
 
 // unsub
-router.delete('following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.unSub);
+router.delete('/following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.unSub);
 
 // sub
-router.post('following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.sub);
+router.post('/following/:userID', auth.isAuth, userValidator.checkIfUserIDExsits, userValidator.checkIfNotSelf, followedController.sub);
 
 module.exports = router;
