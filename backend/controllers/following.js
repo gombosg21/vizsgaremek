c61097f53b9aaa17cc06779340316b74240e77f0
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-10 12:40:32 +0200

    better behavior

--- backend/controllers/following.js
+++ backend/controllers/following.js
@@ -1,10 +1,13 @@
-const user = require("../models").user;
 const followed = require("../models").followed;
 
 exports.getFollowedList = async (req, res, next) => {
     const userID = req.user.ID;
     try {
         const followedList = await followed.findALl({ where: { user_ID: userID } });
+
+        if (!followedList) {
+            return res.status(200).json({ followed_list: "user does not follow anyone" });
+        };
 
         return res.status(200).json({ followed_list: followedList });
     } catch (error) {
@@ -14,7 +17,7 @@ exports.getFollowedList = async (req, res, next) => {
 };
 
 exports.getFollowerCount = async (req, res, next) => {
-    const userID = req.user.ID;
+    const userID = req.params.userID;
     try {
         const followedCount = await followed.count({ where: { followed_ID: userID } });
 
@@ -36,7 +39,7 @@ exports.sub = async (req, res, next) => {
             date: Date.now()
         });
 
-        return res.status(201).json({ subbed: newFollowing });
+        return res.status(201).json({ subbed: { ID: newFollowing.followed_ID } });
     } catch (error) {
         console.error(error);
         return res.status(500);
@@ -49,9 +52,14 @@ exports.unSub = async (req, res, next) => {
     const subID = req.body.id;
     try {
         const deleteFollowing = await followed.findOne({ where: { [Op.and]: [{ user_ID: userID }, { followed_ID: subID }] } });
+
+        if (!deleteFollowing) {
+            return res.status(400).json({ error: `user does not follow user with id: ${subID}` })
+        };
+
         await deleteFollowing.destroy();
 
-        return res.status(200).json({ unsubbed: deleteFollowing });
+        return res.status(200).json({ unsubbed: { ID: deleteFollowing.followed_ID } });
     } catch (error) {
         console.error(error);
         return res.status(500);
