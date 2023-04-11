a2f2f6a25588b4f8bb0ceb6b070ec29864f64030
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-11 20:12:13 +0200

    more sketch

--- backend/controllers/activity.js
+++ backend/controllers/activity.js
@@ -6,7 +6,9 @@ const activity = require("../models").activity;
 exports.getActivity = async (req, res, next) => {
     const userID = req.user.ID;
     try {
-        const activityFeed = activity.findAll({ where: { user_ID: userID } });
+        const activityFeed = activity.findAll({ where: { user_ID: userID }, include: [{ model: friend, include: [{ model: user }] }, { model: followed, include: [{ model: user }] }] });
+
+
 
         // biiig sql query to get every associated users doing, too tired to do it now
     } catch (error) {
@@ -15,10 +17,30 @@ exports.getActivity = async (req, res, next) => {
     };
 };
 
+exports.subNotify = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    };
+};
+
+exports.unSubNotify = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    };
+};
+
 exports.notify = async (req, res, next) => {
-    const userID = req.user.ID;
     try {
         // learn how push notifications work...
+        // needs webSockets
+        // message must be baked dynamically
+        // consult with teammates on howto
     } catch (error) {
         console.log(error);
         return res.status(500);
