afbc8c075e23e03dfb55cf5748371bec107ba5ef
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-12 09:19:27 +0200

    added route namings

--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -3,12 +3,12 @@ const auth = require("../middlewares/authentiaction/auth");
 const router = require('express').Router();
 
 //get user activity
-router.get(auth.isAuth, activityController.getActivity);
+router.get("/activity",auth.isAuth, activityController.getActivity);
 
 //sub notifications
-router.post(auth.isAuth, activityController.subNotify)
+router.post("/notify",auth.isAuth, activityController.subNotify)
 
 //unsub notifiactions
-router.delete(auth.isAuth, activityController.unSubNotify);
+router.delete("/notify",auth.isAuth, activityController.unSubNotify);
 
 module.exports = router;
