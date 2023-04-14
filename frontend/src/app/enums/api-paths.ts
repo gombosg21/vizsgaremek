eba8ac7f4b054af3b6b49a7653efd51b758f2fb1
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-14 16:50:49 +0200

    init

---
+++ frontend/src/app/enums/api-paths.ts
@@ -0,0 +1,11 @@
+export enum ApiPaths {
+    User = 'user',
+    Tags = 'tag',
+    Media = 'media',
+    Thread = 'thread',
+    Comment = 'comment',
+    Story = 'story',
+    Reaction = 'reaction',
+    Friends = 'friends',
+    Follow = 'following'
+};
