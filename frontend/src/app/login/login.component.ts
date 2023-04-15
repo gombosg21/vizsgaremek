7194a13a2eed2e39839103c6e5f51ef7f9d24f89
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-15 14:03:59 +0200

    needs more work

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { User } from '../models/user.model';
+import { AuthService } from '../services/auth/auth.service';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { ApiPaths } from '../enums/api-paths';
+import { enviroment } from 'src/enviroments/enviroment';
 
 import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -22,41 +24,17 @@ export class LoginComponent {
     private router: Router,
     private authService: AuthService,
     private http: HttpClient,
-    private cookieService: CookieService 
   ) {
     this.titleService.setTitle('VisualPosting - Login');
+    ;
   }
-  
-  login() {
-    const user = new User(this.username, this.password);
-    this.authService.loginUser(user).subscribe(
-      (response) => {
-        console.log('Login successful:', response);
-        sessionStorage.setItem('user', JSON.stringify(response));
-        sessionStorage.setItem('token', response.token);
 
-        console.log('Token:', response.token);
-        
-        this.cookieService.set('VSCookie', response.token, { expires: 30, path: '/', sameSite: 'Lax' });
-  
-        console.log('Response object:', response);
-  
-        this.router.navigate([`/profile/${response.ID}`]);
-  
-        this.http.get('/api/v/0.1/user', { observe: 'response' }).subscribe((response) => {
-          const vsCookie = response.headers.get('set-cookie');
-          console.log('VSCookie:', vsCookie);
-        });
-      },
-      (error) => {
-        console.error('Login failed:', error);
-        this.errorMessage = 'Login failed: Invalid username or password';
-        this.password = '';
-      }
-    );
-  }
 
   isFormValid() {
     return this.username && this.password;
-  }
-}
+  };
+
+  login() {
+    this.authService.login(this.username, this.password);
+  };
+};
