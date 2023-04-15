29843db416283ac0b08e07e9053d6c15352b5a8d
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-15 14:37:32 +0200

    better url pathing

--- frontend/src/app/services/register/register.service.ts
+++ frontend/src/app/services/register/register.service.ts
@@ -2,16 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Register } from '../../models/register.model';
+import { enviroment } from 'src/enviroments/enviroment';
+import { ApiPaths } from '../../enums/api-paths';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-  private apiUrl = 'http://localhost:3600/api/v/0.1/user/register';
+  private targetURL = enviroment.baseUrl + ApiPaths.User + "/register";
 
   constructor(private http: HttpClient) {}
 
   registerUser(registerData: Register): Observable<any> {
-    return this.http.post<any>(this.apiUrl, registerData);
-  }
-}
+    return this.http.post<any>(this.targetURL, registerData);
+  };
+};
