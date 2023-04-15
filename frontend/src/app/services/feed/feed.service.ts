9064d3e8e78561b60486aa7406ba03435b013da0
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-15 14:37:56 +0200

    added imports

--- frontend/src/app/services/feed/feed.service.ts
+++ frontend/src/app/services/feed/feed.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from '../../models/post-model';
+import { enviroment } from 'src/enviroments/enviroment';
+import { ApiPaths } from '../../enums/api-paths';
 
 @Injectable({
   providedIn: 'root'
