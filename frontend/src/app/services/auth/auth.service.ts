c935444e6e4621f1c315be91e74071b77f008236
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-16 17:18:14 +0200

    fix

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -3,33 +3,30 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { enviroment } from 'src/enviroments/enviroment';
 import { ApiPaths } from '../../enums/api-paths';
-import { durationStringtoDate } from "src/app/helpers/converters/to_date";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  public LiveSession: boolean = false;
-  public token_expires: Date | undefined = undefined;
-
   constructor(private http: HttpClient) {
   };
 
-  login(username: string, password: string) {
-    this.LiveSession = false;
-    this.token_expires = undefined;
-    this.http.post<any>(enviroment.baseUrl + ApiPaths.User + "/login", { name: username, password: password }).subscribe((data: any) => {
-      sessionStorage.setItem("token", data.token.split(":")[1]);
-      this.LiveSession = true;
-      this.token_expires = durationStringtoDate(data.token_expires);
+  login(username: string, password: string): Observable<any> {
+    const uriResult = this.http.post<any>(enviroment.baseUrl + ApiPaths.User + "/login", { name: username, password: password });
+    uriResult.subscribe({
+      next(data) {
+        sessionStorage.setItem("token", data.token.split(":")[1])
+      },
+      error(err) {
+        console.log(err);
+        throw new Error(err);
+      },
     });
+    return uriResult;
   };
 
   logout(): void {
     this.http.delete<void>(enviroment.baseUrl + ApiPaths.User + "/logout").subscribe(() => {
-      this.LiveSession = false;
-      this.token_expires = undefined;
       sessionStorage.removeItem("token");
     });
   };
@@ -37,5 +34,14 @@ export class AuthService {
   getToken(): string | null {
     const token = sessionStorage.getItem("token");
     return token;
+  };
+
+  activeToken(): Boolean {
+    const token = sessionStorage.getItem("token");
+
+    if (!token) {
+      return false;
+    };
+    return true;
   };
 };
