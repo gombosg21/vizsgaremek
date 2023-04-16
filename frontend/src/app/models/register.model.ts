2a6d5b7fd89f5dd5de95c4fe9d94789c8296e7fa
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-16 20:38:08 +0200

    interface

--- frontend/src/app/models/register.model.ts
+++ frontend/src/app/models/register.model.ts
@@ -1,10 +1,8 @@
-export class Register {
-  constructor(
-    public name: string,
-    public email: string,
-    public password: string,
-    public re_password: string,
-    public gender: number,
-    public birth_date: string
-  ) {}
+export interface Register {
+  name: string,
+  email: string,
+  password: string,
+  re_password: string,
+  gender: number,
+  birth_date: string
 }
