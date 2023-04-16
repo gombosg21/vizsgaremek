fdd216d335937ddea902b7ae4cbcb287515814a2
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-16 20:38:55 +0200

    auth-guard

--- frontend/src/app/services/auth/auth-guard.service.ts
+++ frontend/src/app/services/auth/auth-guard.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
+import { isTokenExpired } from 'src/app/helpers/extractors/token';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanActivate {
 
   constructor(public Router: Router, public AuthService: AuthService) {
 
   }
-  CanActivate(): boolean {
+  canActivate(): boolean {
     if (!this.AuthService.activeToken()) {
       this.Router.navigate(['login']);
       return false;
+    } else {
+      if (isTokenExpired()) {
+        this.Router.navigate(['login']);
+        return false
+      } else {
+        return true;
+      }
     };
-    return true;
   };
 };
