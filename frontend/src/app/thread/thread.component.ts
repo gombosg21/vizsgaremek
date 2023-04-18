61a7969b85bde43775fe3e0efc6329ecbfdbc571
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-18 21:27:45 +0200

    more display

--- frontend/src/app/thread/thread.component.ts
+++ frontend/src/app/thread/thread.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ThreadService } from '../services/thread/thread.service';
 import { comment } from '../models/comment';
 
@@ -7,18 +7,52 @@ import { comment } from '../models/comment';
   templateUrl: './thread.component.html',
   styleUrls: ['./thread.component.css'],
 })
-export class ThreadComponent {
+export class ThreadComponent implements OnInit {
 
-  @Input() public name: string = "";
-  @Input() public created: Date = new Date();
-  @Input() public last_activity: Date = new Date();
-  @Input() public status: number = 0;
+  @Input() public name: string;
+  @Input() public created: Date;
+  @Input() public last_activity: Date;
+  @Input() public status: number;
   @Input() public comments: comment[];
 
+  public threadStatus: string;
+
+
+
   constructor(private ThreadService: ThreadService) {
+
   };
 
+  ngOnInit():void {
+    switch (this.status) {
+      case 0:
+        this.threadStatus = "open"
+        break;
+
+      case 1:
+        this.threadStatus = "locked"
+        break;
+
+      case 2:
+        this.threadStatus = "archived"
+        break;
+
+      default:
+        this.threadStatus = "error"
+        break;
+    }
+  }
+
+
   comment(): void {
+
+  };
+
+  editComment():void {
+
+  };
+
+  deleteComment():void {
 
   };
 
