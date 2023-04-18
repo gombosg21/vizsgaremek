62eec11c0eefb1f8931a852c16b35169b71de29e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-18 19:39:26 +0200

    added comments

--- frontend/src/app/thread/thread.component.ts
+++ frontend/src/app/thread/thread.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ThreadService } from '../services/thread/thread.service';
+import { comment } from '../models/comment';
 
 @Component({
   selector: 'app-thread',
@@ -12,6 +13,7 @@ export class ThreadComponent {
   @Input() public created:Date = new Date();
   @Input() public last_activity:Date = new Date();
   @Input() public status:number = 0;
+  @Input() public comments:comment[];
 
   constructor(private ThreadService: ThreadService) { 
 
