36b8ca70f7799817bf58a9466bcf5416108ff233
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-18 15:30:14 +0200

    basic sketch

--- frontend/src/app/comment/comment.component.ts
+++ frontend/src/app/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommentService } from '../services/comment/comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class CommentComponent {
 
-}
+  constructor(private CommentService: CommentService) {
+
+  };
+
+  content:string = "";
+  created:Date = new Date();
+  last_edit:Date = new Date();
+  user_alias:String = "";
+
+  react():void {
+    console.log("reaction")
+  };
+};
