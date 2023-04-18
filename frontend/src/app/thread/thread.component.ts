6ac346bba5d687909f933f6f0e19960fe30a4a6f
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-18 19:14:52 +0200

    decorators

--- frontend/src/app/thread/thread.component.ts
+++ frontend/src/app/thread/thread.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ThreadService } from '../services/thread/thread.service';
 
 @Component({
   selector: 'app-thread',
   templateUrl: './thread.component.html',
-  styleUrls: ['./thread.component.css']
+  styleUrls: ['./thread.component.css'],
 })
 export class ThreadComponent {
 
-}
+  @Input() public name:string = "";
+  @Input() public created:Date = new Date();
+  @Input() public last_activity:Date = new Date();
+  @Input() public status:number = 0;
+
+  constructor(private ThreadService: ThreadService) { 
+
+  };
+
+  comment():void {
+
+  };
+
+  react():void {
+
+  };
+
+};
