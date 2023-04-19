c8fbc87f5c7436aa59c0ae438cc413ed4dcf0c95
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-19 11:35:54 +0200

    init

---
+++ frontend/src/app/models/media.ts
@@ -0,0 +1,10 @@
+export interface media {
+    ID:number,
+    data:string,
+    created:Date,
+    last_edit:Date,
+    visibility:number,
+    descriptin:string
+    tags?:any[]
+    reactions?: any[]
+};
