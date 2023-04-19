01d000fbe5fe0cac150ee6f33568c85635f0346e
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-19 11:36:06 +0200

    defined logic

--- frontend/src/app/services/media/media.service.ts
+++ frontend/src/app/services/media/media.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { enviroment } from 'src/enviroments/enviroment';
+import { ApiPaths } from '../../enums/api-paths';
+import { Observable } from 'rxjs';
+import { media } from 'src/app/models/media';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaService {
 
-  constructor() { }
+  private ApiPath = enviroment.baseUrl + ApiPaths.Media;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  getAllMediaFromUserID(ID:number):Observable<any> {
+    return this.http.get(this.ApiPath + "/all/" + ID);
+  };
+
+  getOneFromUserID(ID:number):Observable<any> {
+    return this.http.get(this.ApiPath + "/" + ID);
+  };
+
+  uploadMedia(data:media):Observable<any> {
+    return this.http.post(this.ApiPath, data);
+  };
 }
