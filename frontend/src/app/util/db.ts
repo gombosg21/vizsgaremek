0af8d7f4176e1d2893ff9afaf3e0a711fee8849a
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-20 11:37:52 +0200

    needs more work

--- frontend/src/app/util/db.ts
+++ frontend/src/app/util/db.ts
@@ -1,7 +1,42 @@
-import { NgxIndexedDBService, NgxIndexedDBModule } from "ngx-indexed-db";
+import { NgxIndexedDBService, NgxIndexedDBModule, DBConfig } from "ngx-indexed-db";
+import { Injectable, NgModule } from '@angular/core';
 
-export class Db {
 
-    
+const dbConfig: DBConfig = {
+    name: "VisualIndexDB",
+    version: 1.0,
+    objectStoresMeta: [
+        {
+            store: "reactions",
+            storeConfig: {
+                keyPath: 'id',
+                autoIncrement: true,
+            },
+            storeSchema: [
+                {
+                    name: "name", keypath: "name", options: { unique: true }
+                },
+                {
+                    name: "data", keypath: "data", options: { unique: true }
+                }
+            ]
 
+        }, {
+            store: "tags",
+            storeConfig: {
+                keyPath: 'id',
+                autoIncrement: true,
+            }
+            ,
+            storeSchema: [
+                {
+                    name: "name", keypath: "name", options: { unique: true }
+                },
+                {
+                    name: "count", keypath: "count", options: { unique: false }
+                }
+            ]
+        }
+    ]
 }
+
