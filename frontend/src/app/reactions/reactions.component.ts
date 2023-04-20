5cce1d0ae9b13552cc442272fe4bceb79c54e709
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-20 11:36:34 +0200

    defined basic logic

--- frontend/src/app/reactions/reactions.component.ts
+++ frontend/src/app/reactions/reactions.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { reaction } from '../models/reaction';
+import { ReactionService } from '../services/reaction/reaction.service';
 
 @Component({
   selector: 'app-reactions',
   templateUrl: './reactions.component.html',
   styleUrls: ['./reactions.component.css']
 })
+
 export class ReactionsComponent {
 
-}
+  private data: reaction[];
+
+  @Input() reactions: reaction[];
+  @Input() ID: number;
+  @Input() targetType: string;
+
+  constructor(private ReactionService: ReactionService) {
+    this.ReactionService.getAllreactions(this.ID, this.targetType).subscribe({
+      next: (value) => { this.data = value },
+      error(err) {
+        console.log(err);
+      },
+    })
+    this.reactions = this.data ?? this.reactions;
+  };
+};
