c1f7b038afd91d6e4aa9ed094abf4db7d68c61ff
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 15:36:03 +0200

    fix import

--- frontend/src/app/services/feed/feed.service.ts
+++ frontend/src/app/services/feed/feed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Post } from '../../models/post-model';
+import { Post } from '../../models/post';
 import { enviroment } from 'src/enviroments/enviroment';
 import { ApiPaths } from '../../enums/api-paths';
 
