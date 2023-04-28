4cac0f444e79f75a8a9664f69d402dbea0e196ba
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 11:48:49 +0200

    defined logic

--- frontend/src/app/services/tags/tags.service.ts
+++ frontend/src/app/services/tags/tags.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tag } from 'src/app/models/tag';
+import { Observable } from 'rxjs';
+import { enviroment } from 'src/enviroments/enviroment';
+import { ApiPaths } from 'src/app/enums/api-paths';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagsService {
 
-  constructor() { }
+  private ApiPath:string = enviroment.baseUrl + ApiPaths.Tags;
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  getAllTags():Observable<tag[]> {
+    return this.http.get<tag[]>(this.ApiPath + "/all");
+  };
+
+  createTag(tag:tag):Observable<tag> {
+    return this.http.post<tag>(this.ApiPath, tag);
+  };
+
+  editTag(tag:tag):Observable<tag> {
+    return this.http.patch<tag>(this.ApiPath + "/" + tag.id, {name : tag.name})
+  };
+
+  deleteTag(tag:tag):Observable<tag> {
+    return this.http.delete<tag>(this.ApiPath + "/" + tag.id);
+  };
+
+  searchTags(namelike:string):Observable<tag[]> {
+    return this.http.get<tag[]>(this.ApiPath + "?" + "name=" + namelike);
+  };
 }
