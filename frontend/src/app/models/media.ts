c151e8843119c0477adda60a5a7e8516dbcb89fd
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 22:47:03 +0200

    fixed fields

--- frontend/src/app/models/media.ts
+++ frontend/src/app/models/media.ts
@@ -2,13 +2,13 @@ import { tag } from "./tag"
 import { reaction_short } from "./reaction"
 
 export interface media {
-    ID?:number,
-    data:string,
-    created:Date,
-    last_edit:Date,
-    visibility:number,
-    description:string,
-    placeholder:string,
-    tags?:tag[],
+    ID?: number,
+    file_data: string,
+    uploaded: Date,
+    last_edit: Date,
+    visibility: number,
+    description: string,
+    placeholder_text: string,
+    tags?: tag[],
     reactions?: reaction_short[]
 };
