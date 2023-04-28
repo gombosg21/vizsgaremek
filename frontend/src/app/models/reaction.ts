8a2db73cff68d096b6a32882de22bad0e6cad66b
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 21:04:53 +0200

    additional model

--- frontend/src/app/models/reaction.ts
+++ frontend/src/app/models/reaction.ts
@@ -2,9 +2,15 @@ export interface reaction {
     ID: number,
     name: string,
     data: string,
-}
+};
 
 export interface reaction_short {
     ID: number,
     count: number,
-}
+};
+
+export interface reaction_local {
+    ID: number,
+    name: string,
+    data: Blob,
+};
