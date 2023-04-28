c7e066d80e6cac8a22cc7255573bb1b358327514
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 15:37:19 +0200

    added submodels

--- frontend/src/app/models/comment.ts
+++ frontend/src/app/models/comment.ts
@@ -1,7 +1,11 @@
+import { reaction_short } from "./reaction"
+import { user_short } from "./user"
+
 export interface comment {
     ID: number,
     created: Date,
     last_edit: Date
     content: string,
-    comment_reactionlists: number[],
+    comment_reactionlists?: reaction_short[],
+    user: user_short;
 }
