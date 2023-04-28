b3f56580fef7a23596b24f778b7220b7cf10f975
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 21:05:28 +0200

    now stores actual blobs

--- frontend/src/app/services/db/db.service.ts
+++ frontend/src/app/services/db/db.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
-import { Observable, firstValueFrom, lastValueFrom } from 'rxjs'
-import { tag } from 'src/app/models/tag';
-import { reaction } from 'src/app/models/reaction';
+import { Observable, firstValueFrom } from 'rxjs'
+import { reaction_local } from 'src/app/models/reaction';
 import { TagsService } from '../tags/tags.service';
 import { ReactionService } from '../reaction/reaction.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +18,18 @@ export class DbService {
     return this.dbService.bulkDelete("reactions", []);
   };
 
-  getCacheReactions(IDs: number[]): Observable<reaction[]> {
+  getCacheReactions(IDs: number[]): Observable<reaction_local[]> {
     return this.dbService.bulkGet("reactions", IDs);
   };
 
-  getAllCahceReactions():Observable<reaction[]> {
+  getAllCahceReactions(): Observable<reaction_local[]> {
     return this.dbService.getAll("reactions");
   };
 
   async fillReactions(): Promise<Observable<number[]>> {
     const dataSource = this.ReactionsService.getAllReactions()
-    const data = await firstValueFrom(dataSource);
+    const rawData = await firstValueFrom(dataSource);
+    const data:reaction_local[] = rawData.map((reaction) => ({ ID: reaction.ID, name: reaction.name, data: new Blob([atob(reaction.data)])}))
     console.log(data)
     return this.dbService.bulkAdd("reactions", data);
   };
@@ -40,7 +41,6 @@ export class DbService {
   async fillTags(): Promise<Observable<number[]>> {
     const dataSource = this.TagsService.getAllTags();
     const data = await firstValueFrom(dataSource);
-    console.log(data)
     return this.dbService.bulkAdd("tags", data);
   };
 }
