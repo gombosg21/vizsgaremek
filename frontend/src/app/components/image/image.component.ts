dda256247d126424293e0ed0b1789efce6760848
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 22:46:01 +0200

    better logic

--- frontend/src/app/components/image/image.component.ts
+++ frontend/src/app/components/image/image.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MediaService } from '../../services/media/media.service';
 import { media } from '../../models/media';
-import { tag } from 'src/app/models/tag';
-import { reaction_short } from 'src/app/models/reaction';
 
 @Component({
   selector: 'app-image',
@@ -11,41 +9,21 @@ import { reaction_short } from 'src/app/models/reaction';
 })
 export class ImageComponent {
 
-  @Input() data: media;
-  @Input() public ID: number = -1;
-  @Input() public imageData: string;
-  @Input() public created: Date;
-  @Input() public last_edit: Date;
-  @Input() public visibility: number;
-  @Input() public description: string;
-  @Input() public tags?: tag[];
-  @Input() public reactions?: reaction_short[];
-  @Input() public placeholder: string;
+  @Input() mediaID: number;
+  @Input() media: media;
+
 
   constructor(private MediaService: MediaService) {
-    if (this.ID) {
-      this.MediaService.getOneFromUserID(this.ID).subscribe({
+    if (this.mediaID) {
+      this.MediaService.getOneFromUserID(this.mediaID).subscribe({
         next: (value) => {
-          this.data = value;
-
-          this.ID = this.ID ?? this.data.ID;
-          this.imageData = this.imageData ?? this.data.data;
-          this.created = this.created ?? this.data.created;
-          this.last_edit = this.last_edit ?? this.data.last_edit;
-          this.visibility = this.visibility ?? this.data.visibility;
-          this.description = this.description ?? this.data.description;
-          this.tags = this.tags ?? this.data.tags;
-          this.reactions = this.reactions ?? this.data.reactions;
-          this.placeholder = this.placeholder ?? this.data.placeholder;
+          this.media = value;
         },
         error: (err) => {
           console.error(err);
         },
       });
+
     };
-
   };
-
-
-
 };
