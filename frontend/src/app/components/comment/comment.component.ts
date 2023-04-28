d249e865c3dfbaf91d423d63c867f200af559367
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-28 15:37:56 +0200

    reaction instance create

--- frontend/src/app/components/comment/comment.component.ts
+++ frontend/src/app/components/comment/comment.component.ts
@@ -4,6 +4,7 @@ import { ReactionService } from '../../services/reaction/reaction.service';
 import { comment } from '../../models/comment';
 import { reaction } from 'src/app/models/reaction';
 import { DbService } from 'src/app/services/db/db.service';
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-comment',
@@ -12,17 +13,17 @@ import { DbService } from 'src/app/services/db/db.service';
 })
 export class CommentComponent implements OnInit {
 
-  private data:comment;
+  private data: comment;
 
-  @Input() public content:string;
-  @Input() public created:Date;
-  @Input() public last_edit:Date;
-  @Input() public user_alias:string;
-  @Input() public ID:number;  
-  @Input() public iterator:number = 0;
+  @Input() public content: string;
+  @Input() public created: Date;
+  @Input() public last_edit: Date;
+  @Input() public user_alias: string;
+  @Input() public ID: number;
+  @Input() public iterator: number = 0;
   @Input() public reactions: reaction[];
 
-  constructor(private CommentService: CommentService, private ReactionService:ReactionService, private DBService:DbService) {
+  constructor(private CommentService: CommentService, private ReactionService: ReactionService, private DBService: DbService) {
     this.data = this.CommentService.getLocalData()[this.iterator];
   };
 
@@ -33,14 +34,34 @@ export class CommentComponent implements OnInit {
     this.ID = this.data.ID ?? this.ID;
     this.last_edit = this.data.last_edit ?? this.last_edit;
     this.created = this.data.created ?? this.created;
-    this.DBService.getCacheReactions(this.data.comment_reactionlists).subscribe({
-      next: (value) => {
-        this.reactions = value ?? this.reactions;
-      }
-    });
+    if (this.data.comment_reactionlists?.length != 0) {
+      this.DBService.getCacheReactions(this.data.comment_reactionlists!.map((reaction) => (reaction.ID))).subscribe({
+        next: (value) => {
+          this.reactions = value ?? this.reactions;
+        }
+      });
+    }
   }
 
-  react():void {
-    console.log("reaction")
+  react(ID: number): Observable<any> {
+    const reactEvent = this.ReactionService.addReactionInstance(ID, this.ID, "comment");
+
+    reactEvent.subscribe({
+      next: (value) => {
+        this.DBService.getCacheReactions([ID]).subscribe({
+          next: (localValue) => {
+            localValue.forEach(reaction => {
+              this.reactions.push(reaction)
+            });
+          },
+          error: (error) => { console.error(error) }
+        })
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    })
+
+    return reactEvent;
   };
 };
