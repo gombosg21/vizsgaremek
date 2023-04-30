e976716137ebe9a8fcd27825c9bec72ce2d0982d
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-30 22:05:13 +0200

    better match

--- frontend/src/app/models/carousel.ts
+++ frontend/src/app/models/carousel.ts
@@ -1,14 +1,21 @@
 import { media } from "./media"
 import { user_short } from "./user"
+import { reaction_short } from "./reaction"
+import { thread_short } from "./thread"
 
 export interface carousel {
-    user : user_short
+    ID: number,
+    user?: user_short,
+    created_date:Date,
+    modified_date:Date,
     name: string,
     visibility: number,
     description: string,
+    reactions?: reaction_short[],
     carousel_medialist: [{
         item_number: number,
         item_description: string,
         media: media
-    }]
+    }],
+    threads?: thread_short[]
 }
