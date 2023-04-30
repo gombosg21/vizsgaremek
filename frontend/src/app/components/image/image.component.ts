9e3e129ab313d4b2115863b58f125a51356fdf34
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-30 12:23:58 +0200

    local service

--- frontend/src/app/components/image/image.component.ts
+++ frontend/src/app/components/image/image.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MediaService } from '../../services/media/media.service';
 import { media } from '../../models/media';
+import { ReactionService } from 'src/app/services/reaction/reaction.service';
 
 @Component({
   selector: 'app-image',
@@ -9,30 +10,30 @@ import { media } from '../../models/media';
 })
 export class ImageComponent {
 
-  @Input() mediaID: number;
-  @Input() media: media;
-  public reactionIDs: number[];
+  @Input() public mediaID: number;
+  @Input() public media: media;
+  @Input() public iterator: number = 0;
 
-  constructor(private MediaService: MediaService) {
-    if (this.media) {
-      if (this.media.reactions) {
-        this.reactionIDs = this.media.reactions!.map(reaction => reaction.ID);
-      };
-    };
-
+  constructor(private MediaService: MediaService, private ReactionService: ReactionService) {
     if (this.mediaID) {
       this.MediaService.getOneFromUserID(this.mediaID).subscribe({
         next: (value) => {
           this.media = value;
           if (this.media.reactions) {
-            this.reactionIDs = this.media.reactions!.map(reaction => reaction.ID);
+            this.ReactionService.setStoredInstanceList(this.media.reactions);
+            console.log(this.media.reactions)
           };
         },
         error: (err) => {
           console.error(err);
         },
       });
-
+    };
+    if (this.media) {
+      if (this.media.reactions) {
+        this.ReactionService.setStoredInstanceList(this.media.reactions);
+        console.log(this.media.reactions)
+      };
     };
   };
 };
