b4b0d423fc8a7e3bdc2ee41f10d6e9f67f2bf3c8
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-30 21:35:46 +0200

    defined logic

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component , OnInit , Input } from '@angular/core';
+import { carousel } from 'src/app/models/carousel';
+import { reaction_short } from 'src/app/models/reaction';
+import { ReactionService } from 'src/app/services/reaction/reaction.service';
+import { StoryService } from 'src/app/services/story/story.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
 
-}
+  public reactions:reaction_short[];
+
+  @Input() public carousel:carousel;
+  @Input() public iterator:number;
+  @Input() public storyID:number;
+
+  constructor(private CarouselService: StoryService, private ReactionsService: ReactionService) {
+    if(this.storyID) {
+      this.CarouselService.getStoryByID(this.storyID).subscribe({
+        error:(err) => {
+          console.log(err)
+        },
+        next:(value) => {
+          this.carousel = value;
+        },
+        complete:() => {
+        },
+      });
+    };
+  };
+
+  ngOnInit(): void {
+    console.log(this.carousel)
+    if (this.carousel.reactions) {
+      this.reactions = this.carousel.reactions;
+      this.ReactionsService.setStoredInstanceList(this.reactions);
+    };
+  };
+};
