70e417aac1f23d27f5189eb3296a9f7197cf3327
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-04-30 22:54:21 +0200

    modal

--- frontend/src/app/components/modals/reaction-modal/reaction-modal.component.ts
+++ frontend/src/app/components/modals/reaction-modal/reaction-modal.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { reaction } from 'src/app/models/reaction';
 import { DbService } from 'src/app/services/db/db.service';
-import { Output } from '@angular/core';
-
-
+import { ReactionService } from 'src/app/services/reaction/reaction.service';
 
 
 @Component({
@@ -13,22 +11,40 @@ import { Output } from '@angular/core';
 })
 export class ReactionModalComponent {
 
-  @Output() reactionInstance:reaction;
+  @Output() public reactionInstance: reaction;
+  @Input() public parentType: string;
+  @Input() public parentID: number;
 
-  public reactions:reaction[];
+  @Output() reacted = new EventEmitter<reaction>();
+  @Output() canceled = new EventEmitter<void>();
 
-  constructor(private DBServie:DbService) {
+
+  public selectedIterator: number;
+  public reactions: reaction[];
+
+  constructor(private DBServie: DbService, private ReactionsService: ReactionService) {
     this.DBServie.getAllCahceReactions().subscribe({
-      next:(value) => {
+      next: (value) => {
         this.reactions = value;
       },
-      error:(err) => {
+      error: (err) => {
         console.error(err)
       },
     })
   };
 
-  add():void {
-  }
+  add(): void {
+    this.ReactionsService.addReactionInstance(this.selectedIterator + 1, this.parentID, this.parentType).subscribe({
+      error: (error) => {
+        console.log(error)
+      },
+      next: (value) => {
+        this.reacted.emit(this.reactions[this.selectedIterator]);
+      },
+      complete: () => {
+
+      }
+    });
+  };
 
 };
