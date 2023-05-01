7d1598a8a5d1f1ba94524ddb06e5c401c964b992
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-01 10:58:41 +0200

    fixme

--- frontend/src/app/components/media-tileset/media-tileset.component.ts
+++ frontend/src/app/components/media-tileset/media-tileset.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { media } from 'src/app/models/media';
 import { MediaService } from 'src/app/services/media/media.service';
 import { getTokenUserID } from '../../helpers/extractors/token';
@@ -8,13 +8,54 @@ import { getTokenUserID } from '../../helpers/extractors/token';
   templateUrl: './media-tileset.component.html',
   styleUrls: ['./media-tileset.component.css']
 })
-export class MediaTilesetComponent {
+export class MediaTilesetComponent implements OnInit {
+
+  @Input() userID: number;
+  @Input() medias: media[];
+
+  private windowWidth: number = window.innerWidth;
+
+  public cols: number;
 
   constructor(private MediaService: MediaService) {
     if (this.userID == undefined) {
       this.userID = getTokenUserID()
     };
+    switch (true) {
+      case (this.windowWidth > 2000): {
+        this.cols = 6;
+        break;
+      }
+      case (this.windowWidth > 1600): {
+        this.cols = 5;
+        break;
+      }
+      case (this.windowWidth > 1200): {
+        this.cols = 4;
+        break;
+      }
+      case (this.windowWidth > 800): {
+        this.cols = 3;
+        break;
+      }
+      case (this.windowWidth > 800): {
+        this.cols = 2;
+        break;
+      }
+      case (this.windowWidth < 801): {
+        this.cols = 1;
+        break;
+      }
+      default: {
+        this.cols = 1;
+        console.error({ error: "cannot acces display size, using default column counts" })
+      }
+    };
+  };
 
+
+
+  ngOnInit(): void {
     if (this.userID) {
       this.MediaService.getAllMediaFromUserID(this.userID).subscribe({
         next: (value) => {
@@ -22,53 +63,14 @@ export class MediaTilesetComponent {
             this.medias = value ?? this.medias;
             this.MediaService.setLocalMediaList(this.medias);
           }
-          switch (true) {
-            case (this.windowWidth < 2000): {
-              this.cols = 7;
-              break;
-            }
-            case (this.windowWidth < 1600): {
-              this.cols = 6;
-              break;
-            }
-            case (this.windowWidth < 1200): {
-              this.cols = 5;
-              break;
-            }
-            case (this.windowWidth < 1000): {
-              this.cols = 4;
-              break;
-            }
-            case (this.windowWidth < 800): {
-              this.cols = 3;
-              break;
-            }
-            case (this.windowWidth < 600): {
-              this.cols = 2;
-              break;
-            }
-            case (this.windowWidth < 400): {
-              this.cols = 1;
-              break;
-            }
-            default: {
-              this.cols = 1;
-              console.error({ error: "cannot acces display size, using default column counts" })
-            }
-          }
         },
         error: (err) => {
           console.error(err);
         }
       });
     };
+    console.log("a", this.cols)
+    console.log(this.windowWidth);
   };
 
-  @Input() userID: number;
-  @Input() medias: media[];
-
-  private windowWidth: number = window.innerWidth;
-
-  public cols: number = 1;
-
-}
+};
