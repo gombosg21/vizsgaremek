95d7d463fcfa33f91eef08c39e87027e0e73cf2b
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-01 10:57:37 +0200

    needs more work

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
-import { Component , OnInit , Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { carousel } from 'src/app/models/carousel';
+import { media } from 'src/app/models/media';
 import { reaction_short } from 'src/app/models/reaction';
+import { MediaService } from 'src/app/services/media/media.service';
 import { ReactionService } from 'src/app/services/reaction/reaction.service';
 import { StoryService } from 'src/app/services/story/story.service';
 
@@ -11,33 +13,35 @@ import { StoryService } from 'src/app/services/story/story.service';
 })
 export class CarouselComponent implements OnInit {
 
-  public reactions:reaction_short[];
+  public reactions: reaction_short[];
 
-  @Input() public carousel:carousel;
-  @Input() public iterator:number;
-  @Input() public storyID:number;
+  @Input() public carousel: carousel;
+  @Input() public iterator: number;
+  @Input() public storyID: number;
 
-  constructor(private CarouselService: StoryService, private ReactionsService: ReactionService) {
-    if(this.storyID) {
+  constructor(private CarouselService: StoryService, private ReactionsService: ReactionService, private MediaService: MediaService) {
+    if (this.storyID) {
       this.CarouselService.getStoryByID(this.storyID).subscribe({
-        error:(err) => {
-          console.log(err)
+        error: (err) => {
+          console.error(err)
         },
-        next:(value) => {
-          this.carousel = value;
-          this.carousel.carousel_medialist[0].media
+        next: (value) => {
+          this.carousel = value ?? this.carousel;
         },
-        complete:() => {
+        complete: () => {
         },
       });
     };
   };
 
   ngOnInit(): void {
-    console.log(this.carousel)
     if (this.carousel.reactions) {
       this.reactions = this.carousel.reactions;
       this.ReactionsService.setStoredInstanceList(this.reactions);
     };
+    if (this.carousel.carousel_medialist) {
+      const mediaList:media[] = this.carousel.carousel_medialist.map(instance => instance.media);
+      this.MediaService.setLocalMediaList(mediaList);
+    }
   };
 };
