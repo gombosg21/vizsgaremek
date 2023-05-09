19089c64035e5ee58e3316c297f58014c45aaaaa
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-09 12:31:49 +0200

    removed usued imports

--- frontend/src/app/components/media-thread/media-thread.component.ts
+++ frontend/src/app/components/media-thread/media-thread.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { media } from 'src/app/models/media';
-import { thread } from 'src/app/models/thread';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { MediaService } from 'src/app/services/media/media.service';
-import { ThreadService } from 'src/app/services/thread/thread.service';
 
 @Component({
   selector: 'app-media-thread',
@@ -15,7 +10,7 @@ export class MediaThreadComponent implements OnInit, OnDestroy {
   public mediaID?: number;
   public threadID?: number;
 
-  constructor(private router: ActivatedRoute, private Auth: AuthService, private ThreadService: ThreadService, private MediaService: MediaService) {
+  constructor(private router: ActivatedRoute) {
   };
 
   ngOnInit(): void {
