c226fa2d18608476a1446304cf970aaceb1a57c1
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-09 19:21:22 +0200

    dragndrop

--- frontend/src/app/components/carousel-builder/carousel-builder.component.ts
+++ frontend/src/app/components/carousel-builder/carousel-builder.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { media } from 'src/app/models/media';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { MediaService } from 'src/app/services/media/media.service';
 import { StoryService } from 'src/app/services/story/story.service';
+import { CdkDragDrop, moveItemInArray, copyArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-carousel-builder',
@@ -10,10 +13,9 @@ import { StoryService } from 'src/app/services/story/story.service';
 })
 export class CarouselBuilderComponent implements OnInit {
 
-  @Input() media: media;
-  @Output() cancelled: EventEmitter<void> = new EventEmitter<void>;
-  public displayModal: boolean = false;
-  private selectedIndex: number;
+  @Output() cancelled: EventEmitter<void> = new EventEmitter<void>();
+  public medias: media[] = [];
+  public slides: { item_description: string, media: media }[]= [];
 
   public storyNewFormGroup = new FormGroup({
     nameFormControl: new FormControl<string>('', Validators.compose([Validators.required])),
@@ -28,36 +30,49 @@ export class CarouselBuilderComponent implements OnInit {
     mediaDescriptionFormControl: new FormControl<string>('', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(300)]))
   });
 
-  constructor(private StoryService: StoryService) { };
+  constructor(private StoryService: StoryService, private Auth: AuthService, private MediaService: MediaService) { };
 
-  addMediaID(mediaID: number): void {
-    this.displayModal = false;
-    console.log( 'a',this.storyNewFormGroup.value.mediaArrayControl![this.selectedIndex].value)
-    this.storyNewFormGroup.value.mediaArrayControl![this.selectedIndex].value.mediaIDFormControl = mediaID;
+  ngOnInit(): void {
+    this.MediaService.getAllMediaFromUserID().subscribe({
+      next: (val) => {
+        val.forEach(item => {
+          if (Object.hasOwn(item, 'ID')) {
+            this.medias.push(item as media);
+          };
+        });
+      }, error: (err) => {
+        console.error(err);
+      },
+      complete: () => { }
+    });
   };
 
-  ngOnInit(): void { };
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      var dragItem = event.previousContainer.data[event.previousIndex];
+      if(Object.hasOwn(dragItem,"item_description")) {
+        delete dragItem.item_description;
+        dragItem = dragItem.media as media;
+      } else {
+        dragItem = {
+          media: dragItem,
+          item_description: "",
+        }
+        dragItem = dragItem as { item_description: string, media: media };
+      };
+      console.log(event)
 
-  addFormArrayGroupItem() {
-    this.storyNewFormGroup.value.mediaArrayControl!.push(this.mediaItemFormGroup);
+      copyArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    };
   };
 
-  removeFormArrayGroupItem(group: FormGroup) {
-    this.storyNewFormGroup.value.mediaArrayControl!.splice(this.storyNewFormGroup.value.mediaArrayControl!.indexOf(group), 1);
-  };
-
-  editFormArrayGroupItem(group: FormGroup, index: number) {
-    this.storyNewFormGroup.value.mediaArrayControl![index] = group;
-  };
-
-  showModal(selectedIndex: number): void {
-    this.selectedIndex = selectedIndex;
-    this.displayModal = true;
-  };
-
-  hideModal(): void {
-    this.displayModal = false;
-  };
 
   onSubmit(): void {
     console.log(this.storyNewFormGroup.value)
