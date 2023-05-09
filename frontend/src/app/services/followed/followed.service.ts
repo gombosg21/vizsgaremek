2dac3fcbec695470482448212fd3ac08e75be675
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-09 22:01:45 +0200

    fix

--- frontend/src/app/services/followed/followed.service.ts
+++ frontend/src/app/services/followed/followed.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription, throwError } from 'rxjs';
+import { Observable, Subscription, throwError, of } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { ApiPaths } from 'src/app/enums/api-paths';
 import { enviroment } from 'src/enviroments/enviroment';
@@ -15,19 +15,22 @@ export class FollowedService implements OnInit, OnDestroy {
   private userSub: Subscription;
   private sessionId?: number;
 
-  constructor(private http: HttpClient, private Auth: AuthService) {
-    this.userSub = this.Auth.getUserID().subscribe({
-      error: (err) => { console.error(err) },
-      next: (val) => {
-        this.sessionId = val;
-      },
-      complete: () => { }
-    });
+  private subList: Observable<any>;
+
+  constructor(private http: HttpClient, private Auth: AuthService) { };
+
+  get getLocalSubsList(): Observable<any> {
+    return this.subList;
+  };
+
+  set setLocalSubsList(sublist: any[]) {
+    this.subList = of(sublist);
   };
 
   postSub(targetUserID: number): Observable<any> {
     return this.http.post(this.apiPath + "/" + targetUserID, "");
   };
+
   deleteUnsub(targetUserID: number): Observable<any> {
     return this.http.delete(this.apiPath + "/" + targetUserID);
   };
@@ -40,20 +43,25 @@ export class FollowedService implements OnInit, OnDestroy {
     };
   };
 
-
-  getSubCount(userID?: number) {
+  getSubCount(userID?: number): Observable<number> {
     if (this.sessionId || userID) {
       const ID = userID ?? this.sessionId;
-      return this.http.get(this.apiPath + "/" + ID);
+      return this.http.get<number>(this.apiPath + "/count/" + ID);
     } else {
       return throwError(() => { new Error("was called w/o a valid session or a userID!") });
     };
   };
 
-
   ngOnInit(): void {
+    this.userSub = this.Auth.getUserID.subscribe({
+      error: (err) => { console.error(err) },
+      next: (val) => {
+        this.sessionId = val;
+      },
+      complete: () => { }
+    });
   };
-  
+
   ngOnDestroy(): void {
     if (this.userSub) {
       this.userSub.unsubscribe();
