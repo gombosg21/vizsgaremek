c456d88e2b76f35ec98c288f9cadc5cbf57b0f4c
Selmeczi Máté Vilmos <smat3us@gmail.com>
2023-05-09 12:32:04 +0200

    defined logic

--- frontend/src/app/components/carousel-thread/carousel-thread.component.ts
+++ frontend/src/app/components/carousel-thread/carousel-thread.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-carousel-thread',
   templateUrl: './carousel-thread.component.html',
   styleUrls: ['./carousel-thread.component.css']
 })
-export class CarouselThreadComponent {
+export class CarouselThreadComponent implements OnDestroy, OnInit {
 
-}
+  public carouselID?: number;
+  public threadID?: number;
+
+  constructor(private router: ActivatedRoute) {
+  };
+
+  ngOnInit(): void {
+    this.carouselID = this.router.snapshot.params['carouselid'];
+    this.threadID = this.router.snapshot.params['threadid'];
+  };
+
+  ngOnDestroy(): void { };
+
+};
